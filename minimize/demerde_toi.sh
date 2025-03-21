2a4d39bb3cb9a979c6670cadb2d02fab7a430c98
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-21 17:13:19 +0000

    important update

--- minimize/demerde_toi.sh
+++ minimize/demerde_toi.sh
@@ -35,18 +35,18 @@ fi
 echo "q=\$(${mkt} -d)"
 echo "cd \$q"
 
-echo "${tig} clone https://github.com/senescent777/some_scripts"
-echo "cd some_scripts/minimize"
+echo "${tig} clone https://github.com/senescent777/minimize-devuan-live"
+echo "cd minimize-devuan-live"
 
 #VAIH:ao- if-blkkiin liittyen jos poistaisi ghubista minimize-hamistosta välistä sen /h/d-osuuden
-if [ -d /home/devuan/Desktop/minimize ] ; then #HUOM. pitäisköhän tämä muuttaa?
+if [ -d ~/Desktop/minimize ] ; then #HUOM. pitäisköhän tämä muuttaa?
 	echo "shred -fu ~/Desktop/minimize/* "
 	echo "rm -rf ~/Desktop/minimize/*"
-	echo "mv ./home/devuan/Desktop/minimize/* ~/Desktop/minimize"
+	echo "mv minimize/* ~/Desktop/minimize"
 fi
 
 echo "sudo chmod 0755 ~/Desktop/minimize/${1}"
 echo "sudo chmod 0755 ~/Desktop/minimize/${1}/*.sh"
 
 echo "cd ~/Desktop/minimize"
-echo "./${1}/export.sh 0 /tmp/vomit.tar <param3>"
+echo "./${1}/export.sh 0 /tmp/vomit.tar ${1}"
