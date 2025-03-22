06ecb2b795a0525053f0b1f92a48459490cc9a82
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-22 17:16:20 +0000

    mostky abt 3

--- minimize/chimaera/lib.sh
+++ minimize/chimaera/lib.sh
@@ -21,35 +21,35 @@ function pre_part3() {
 	${NKVD} ${1}/perl-modules-*.deb
 }
 
-function clouds_case0() {
-	dqb  "lib.case0 "
-
-	${slinky} /etc/resolv.conf.OLD /etc/resolv.conf
-	${slinky} /etc/dhcp/dhclient.conf.OLD /etc/dhcp/dhclient.conf
-	${spc} /sbin/dhclient-script.OLD /sbin/dhclient-script
-
-	#samaan tapaan voisi menneä daeDALuksenkin kanssa? s.e. jos a-f - ketjut olemassa ni muutetaan vain dns-säännlt, muussa tapaux pakotetaan rules.v4
-	${ipt} -A INPUT -p udp -m udp --sport 53 -j b 
-	${ipt} -A OUTPUT -p udp -m udp --dport 53 -j e
-	for s in $(grep -v '#' /etc/resolv.conf.OLD | grep names | grep -v 127. | awk '{print $2}') ; do dda_snd ${s} ; done	
-
-	csleep 1
-	dqb "... d0n3"	
-}
-
-function clouds_case1() {
-	echo "lib.case1 (TODO)"
-#		${slinky} /etc/resolv.conf.new /etc/resolv.conf
-#		${slinky} /etc/dhcp/dhclient.conf.new /etc/dhcp/dhclient.conf
-#		${spc} /sbin/dhclient-script.new /sbin/dhclient-script
-#		
-#		${ipt} -A INPUT -p tcp -m tcp --sport 853 -j b
-#		${ipt} -A OUTPUT -p tcp -m tcp --dport 853 -j e
-#		for s in $(grep -v '#' /home/stubby/.stubby.yml | grep address_data | cut -d ':' -f 2) ; do tod_dda ${s} ; done
-
-	csleep 1
-	dqb "... d0n3"	
-}
+#function clouds_case0() {
+#	dqb  "lib.case0 "
+#
+#	${slinky} /etc/resolv.conf.OLD /etc/resolv.conf
+#	${slinky} /etc/dhcp/dhclient.conf.OLD /etc/dhcp/dhclient.conf
+#	${spc} /sbin/dhclient-script.OLD /sbin/dhclient-script
+#
+#	#samaan tapaan voisi menneä daeDALuksenkin kanssa? s.e. jos a-f - ketjut olemassa ni muutetaan vain dns-säännlt, muussa tapaux pakotetaan rules.v4
+#	${ipt} -A INPUT -p udp -m udp --sport 53 -j b 
+#	${ipt} -A OUTPUT -p udp -m udp --dport 53 -j e
+#	for s in $(grep -v '#' /etc/resolv.conf.OLD | grep names | grep -v 127. | awk '{print $2}') ; do dda_snd ${s} ; done	
+#
+#	csleep 1
+#	dqb "... d0n3"	
+#}
+#
+#function clouds_case1() {
+#	echo "lib.case1 (TODO)"
+##		${slinky} /etc/resolv.conf.new /etc/resolv.conf
+##		${slinky} /etc/dhcp/dhclient.conf.new /etc/dhcp/dhclient.conf
+##		${spc} /sbin/dhclient-script.new /sbin/dhclient-script
+##		
+##		${ipt} -A INPUT -p tcp -m tcp --sport 853 -j b
+##		${ipt} -A OUTPUT -p tcp -m tcp --dport 853 -j e
+##		for s in $(grep -v '#' /home/stubby/.stubby.yml | grep address_data | cut -d ':' -f 2) ; do tod_dda ${s} ; done
+#
+#	csleep 1
+#	dqb "... d0n3"	
+#}
 
 dqb "BIL-UR-SAG"
 check_binaries ${distro}
