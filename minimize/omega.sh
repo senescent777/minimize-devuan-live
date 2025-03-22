dc048db0ea06fb3078ec96bd7dd878522101020c
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-22 17:18:38 +0000

    cleaning up

--- minimize/omega.sh
+++ minimize/omega.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-#KIKKAILUT ROSKIKSEEN 666!
-#
-#d=$(dirname $0)
-#echo "VERY MUCH UNDER CONSTRUCTION"
-#echo "VAIH: se sudotus-selvitys"
-#if [ -s ${d}/conf ] && [ -s ${d}/lib.sh ] ; then
-#	. ${d}/conf
-#	. ${d}/lib.sh
-#else
-#	echo "TO CONTINUE FURTHER IS POINTLESS, ESSENTIAL FILES MISSING"
-#	exit 111	
-#fi
-#260125: vaikuttaisi toimivan jnkn verran, ainakin sisään loggaus onnistuu
-#
-##. ${d}/${1}/conf
-#. ${d}/common_lib.sh
-
 debug=1
 odio=$(which sudo)
 smr=$(sudo which rm)
