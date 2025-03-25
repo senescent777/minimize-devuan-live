ac8377f42e8a31db9b7fe89262d6c2913dae93e4
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-25 13:22:20 +0000

    there may be something wrong with part1 (or pre_enforce)

--- minimize/chimaera/pas2.sh
+++ minimize/chimaera/pas2.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 mode=${1}
+n=$(whoami)
+.  ~/Desktop/minimize/chimaera/conf
+#HUOM.250325:onnistuukohan ihan näin pienellä rikkoa chimaera?
 
-case ${1}
+case ${1} in #to state the obvious:if parempi tämmöiseen qu case
 	0)
 		sleep 5
 	;;
-	case 1)
+	1)
 		. ~/Desktop/minimize/common_lib.sh
 		sleep 5
 	;;
-	case 2)
+	2)
 		. ~/Desktop/minimize/common_lib.sh
-		.  ~/Desktop/minimize/chimaera/lib.sh
+		. ~/Desktop/minimize/chimaera/lib.sh
 		sleep 5
+	;;
+	3)
+		#HUOM.25325;tämä taisi vielä pelata
+		. ~/Desktop/minimize/common_lib.sh
+		. ~/Desktop/minimize/chimaera/lib.sh
+		enforce_access ${n} 
+		sleep 5
+	;;
+	4)
+		#tässä taas part1 laittoi kusemaan, ilmankin pre_enforcea, luulisin
+		. ~/Desktop/minimize/common_lib.sh
+		. ~/Desktop/minimize/chimaera/lib.sh
+		
+		pre_enforce ${n} chimaera 
+		enforce_access ${n}
+		part1 chimaera
+		sleep 5
+	;;
+	*)
+		echo "MEE VITTUUN"
 	;;
 esac
 
