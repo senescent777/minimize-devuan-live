d70176a63b8e1d55a29c91566d7f8f07cee7e298
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-25 19:10:37 +0000

    re-wrote this script

--- minimize/chimaera/pas2.sh
+++ minimize/chimaera/pas2.sh
@@ -1,43 +1,20 @@
 #!/bin/bash
-mode=${1}
 n=$(whoami)
-.  ~/Desktop/minimize/chimaera/conf
-#HUOM.250325:onnistuukohan ihan näin pienellä rikkoa chimaera?
+mode=0
 
-case ${1} in #to state the obvious:if parempi tämmöiseen qu case
-	0)
-		sleep 5
-	;;
-	1)
-		. ~/Desktop/minimize/common_lib.sh
-		sleep 5
-	;;
-	2)
-		. ~/Desktop/minimize/common_lib.sh
-		. ~/Desktop/minimize/chimaera/lib.sh
-		sleep 5
-	;;
-	3)
-		#HUOM.25325;tämä taisi vielä pelata
-		. ~/Desktop/minimize/common_lib.sh
-		. ~/Desktop/minimize/chimaera/lib.sh
-		enforce_access ${n} 
-		sleep 5
-	;;
-	4)
-		#tässä taas part1 laittoi kusemaan, ilmankin pre_enforcea, luulisin
-		. ~/Desktop/minimize/common_lib.sh
-		. ~/Desktop/minimize/chimaera/lib.sh
-		
-		pre_enforce ${n} chimaera 
-		enforce_access ${n}
-		part1 chimaera
-		sleep 5
-	;;
-	*)
-		echo "MEE VITTUUN"
-	;;
-esac
+if [ $# -gt 0 ] ; then
+	mode=${1}
+fi
 
+#HUOM.250325:onnistuukohan ihan näin pienellä rikkoa chimaera/x/slim? toivoisi ettei
+#pari ensimmäistä arvausta on jotta part1 tai pre_enforce paskovat asioita
+
+[ ${mode} -gt 1 ] && . ~/Desktop/minimize/chimaera/conf
+[ ${mode} -gt 2 ] && . ~/Desktop/minimize/common_lib.sh
+[ ${mode} -gt 3 ] && . ~/Desktop/minimize/chimaera/lib.sh
+[ ${mode} -gt 4 ] && pre_enforce ${n} chimaera
+[ ${mode} -gt 5 ] && enforce_access ${n}
+[ ${mode} -gt 6 ] && part1 chimaera
+[ ${mode} -gt 0 ] && sleep 5
 
 pkill --signal 9 xfce4-session
