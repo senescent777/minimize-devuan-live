dab0417f10289fd689186052e23b5a7fe13ce646
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-29 17:00:55 +0000

    a new version of omega

--- minimize/omega.sh
+++ minimize/omega.sh
@@ -6,9 +6,17 @@ smr=$(sudo which rm)
 smr="${odio} ${smr} "
 
 whack=$(sudo which pkill)
-#parempi sudotettuna vai ilman?
-whack="${odio} ${whack} --signal 9 "
-mode=1
+#pkill parempi sudotettuna vai ilman? vissiin ilman
+whack="${whack} --signal 9 "
+#whack="${odio} ${whack}  "
+
+svm=$(${odio} which mv)
+svm="${odio} ${svm}"
+mode=3
+
+if [ $# -gt 0 ] ; then
+	mode=${1}
+fi
 
 function dqb() {
 	[ ${debug} -eq 1 ] && echo ${1}
@@ -18,8 +26,7 @@ function csleep() {
 	[ ${debug} -eq 1 ] && sleep ${1}
 }
 
-${odio} usermod -G devuan,cdrom,floppy,audio,dip,video,plugdev,netdev devuan 
-${smr} /etc/sudoers.d/live
-
-${whack} xfce4-session
-#sössön sössön stna
+[ ${mode} -gt 1 ] && ${odio} usermod -G devuan,cdrom,floppy,audio,dip,video,plugdev,netdev devuan 
+[ ${mode} -gt 2 ] && ${smr} /etc/sudoers.d/live
+[ ${mode} -gt 3 ] && ${svm} /etc/sudoers_new /etc/sudoers
+[ ${mode} -gt 0 ] && ${whack} xfce4-session
