74348cf8064348a9523d3a02fb37f4361bc2bd94
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-04-04 16:17:46 +0000

    Add files via upload

--- minimize/wrapper.sh
+++ minimize/wrapper.sh
@@ -3,28 +3,28 @@ distro=$(cat /etc/devuan_version)
 d=$(dirname $0)
 . ${d}/${distro}/conf
 
-#TODO:ehco josqs pois ympäriltä
+#VAIH:ehco josqs pois ympäriltä
 
 case ${1} in
 	merde)
-		echo "${d}/demerde_toi.sh main"		
+		${d}/demerde_toi.sh main		
 	;;
 	cdns)		
-		echo "sudo ${d}/changedns.sh ${dnsm}"
+		sudo ${d}/changedns.sh ${dnsm}
 	;;
 	ifup)
-		echo "sudo /sbin/ifup ${iface}"
+		sudo /sbin/ifup ${iface}
 	;;
 	ifdown)
-		echo "sudo /sbin/ifdown ${iface}"
+		sudo /sbin/ifdown ${iface}
 	;;
 	import)
 		echo "TODO:smthng w/ import2.sh" #HUOM.010425:tässä tarvitaaan käli kysymään ainakin mikä tdsto tuodaan
 	;;
 	doit)
-		echo "${d}/${distro}/doIt6.sh"
+		${d}/${distro}/doIt6.sh
 	;;
 	pt2)
-		echo "${d}/${distro}/pt2.sh"
+		${d}/${distro}/pt2.sh
 	;;	
 esac
