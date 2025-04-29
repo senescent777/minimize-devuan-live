b6bee067bdc11ca4b821e74441e2cf4b809439f0
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-04-29 22:50:32 +0000

    shape of thinsg to cmoe

---
+++ minimize/middleware.sh
@@ -0,0 +1,10 @@
+#jatkossa sitten common_lib käyttöön
+
+function whack() {
+	sudo /usr/bin/pkill --signal 9 $1
+}
+
+function prepare() {
+	tmpdir=$(mktemp -d)
+	[ -s $1 ] && tar -C $tmpdir -xvf $1
+}
