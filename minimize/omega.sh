31172e51bbdf6b09c93ecba1972dbcb7c7f24fca
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-04-29 17:18:48 +0000

    mostly abt cleaning up

--- minimize/omega.sh
+++ minimize/omega.sh
@@ -6,10 +6,7 @@ smr=$(sudo which rm)
 smr="${odio} ${smr} "
 
 whack=$(sudo which pkill)
-#pkill parempi sudotettuna vai ilman? vissiin ilman
 whack="${whack} --signal 9 "
-#whack="${odio} ${whack}  "
-
 svm=$(${odio} which mv)
 svm="${odio} ${svm}"
 mode=3
@@ -17,14 +14,6 @@ mode=3
 if [ $# -gt 0 ] ; then
 	mode=${1}
 fi
-
-#function dqb() {
-#	[ ${debug} -eq 1 ] && echo ${1}
-#}
-#
-#function csleep() {
-#	[ ${debug} -eq 1 ] && sleep ${1}
-#}
 
 [ ${mode} -gt 1 ] && ${odio} usermod -G devuan,cdrom,floppy,audio,dip,video,plugdev,netdev devuan 
 [ ${mode} -gt 2 ] && ${smr} /etc/sudoers.d/live
