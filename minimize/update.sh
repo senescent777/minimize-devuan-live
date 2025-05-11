70c3a2225c26508b516c3167b4c975ffa58c6492
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-11 21:05:57 +0000

    mount and umount

--- minimize/update.sh
+++ minimize/update.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+distro=$(cat /etc/devuan_version) #voisi olla komentoriviparametrikin jatkossa?
+u=0
+
+if [ z"${distro}" != "z" ] ; then
+	if [ -s ~/Desktop/minimize/${distro}/conf ] ; then
+		. ~/Desktop/minimize/${distro}/conf
+		[ -d ${dir} ] && mount ${dir}
+		[ $? -eq 0 ] && u=1
+	fi
+fi
+
 tgt=${1}
 [ -f ${tgt} ] || exit 1
 
-#TODO:tähän kenties jotain muutoksia, liittyen fstab:in sorkkimiseen
-for f in $(find ~/Desktop/minimize/ -name 'conf*') ; do sudo tar -f ${tgt} -rv ${f} ; done
-for f in $(find ~/Desktop/minimize/ -name '*.sh') ; do sudo tar -f ${tgt} -rv ${f} ; done
+for f in $(find ~/Desktop/minimize/ -name 'conf*') ; do tar -f ${tgt} -rv ${f} ; done
+for f in $(find ~/Desktop/minimize/ -name '*.sh') ; do tar -f ${tgt} -rv ${f} ; done
+for f in $(find /etc -name 'locale*') ; do tar -f ${tgt} -rv ${f} ; done
+tar -f ${tgt} -rv /etc/timezone
 
-sudo tar -f ${tgt} -rv /etc/default/locale* /etc/timezone
+[ ${u} -eq 1 ] && umount ${dir}
