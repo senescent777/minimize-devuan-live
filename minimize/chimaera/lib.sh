110b1b7a671ef87f279a61e4c61068748f5979a8
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-15 00:28:52 +0300

    mosty abt improving readability

--- minimize/chimaera/lib.sh
+++ minimize/chimaera/lib.sh
@@ -1,20 +1,31 @@
 #=========================PART 0 ENDS HERE=================================================================
 function pr4() {
-psqa ${1}
-#TODO:dnsm-jutuille jos tekisijojo tain , esim. stubby+dnsmasq toimimaan ni ei tartte noita poistella
-if [ ${dnsm} -eq 1 ] ; then
-${NKVD} ${1}/stubby*
-${NKVD} ${1}/libgetdns*
-${NKVD} ${1}/dnsmasq*
-fi
+	psqa ${1}
+	#TODO:dnsm-jutuille jos tekisijojo tain , esim. stubby+dnsmasq toimimaan ni ei tartte noita poistella
+
+	if [ ${dnsm} -eq 1 ] ; then #TODO:dnsm paraetriksi
+		${NKVD} ${1}/stubby*
+		${NKVD} ${1}/libgetdns*
+		${NKVD} ${1}/dnsmasq*
+	fi
 }
+
 function pre_part3() {
-if [ ${dnsm} -eq 1 ] ; then
-${sdi} ${1}/dns-root-data*.deb
-${NKVD} ${1}/dns-root-data*.deb
-fi
-${sdi} ${1}/perl-modules-*.deb
-${NKVD} ${1}/perl-modules-*.deb
+	dqb "PER3"
+	csleep 1
+
+	if [ ${dnsm} -eq 1 ] ; then #TODO:dnsm parametriksi
+		${sdi} ${1}/dns-root-data*.deb
+		${NKVD} ${1}/dns-root-data*.deb
+	fi
+
+	csleep 1
+
+	${sdi} ${1}/perl-modules-*.deb
+	${NKVD} ${1}/perl-modules-*.deb
+
+	dqb "PER3.D0EN"
+	csleep 1
 }
 
 #TODO:testaa tämän toiminta
