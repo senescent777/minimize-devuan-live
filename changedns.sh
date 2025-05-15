20fed123d5adfd20fe48608efd2db6310dd7ff3f
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-15 12:01:19 +0300

    more fixes

---
+++ changedns.sh
@@ -0,0 +1,327 @@
+#!/bin/bash
+debug=0
+mode=-1
+distro=$(cat /etc/devuan_version)
+
+chmod a-wx ./clouds*
+chown root:root ${0}
+chmod 0555 ${0}
+
+function dqb() {
+	[ ${debug} -eq 1 ] && echo ${1}
+}
+
+function csleep() {
+	[ ${debug} -eq 1 ] && sleep ${1}
+}
+
+#function pr4() { tarpeellisia?
+#	dqb "cdns.pr4 (${1})" 
+#}
+#
+#function pre_part3() {
+#	dqb "cdns.pre_part3( ${1})"
+#}
+#
+#asiasta kukkaruukkuun: wicd oli aikoinaan siedettävä softa, ainakin Networkmanageriin verrattuna
+
+#HUOM.jatkossa ehkä parempi että komentorivioptioilla ei aktivoida debugia
+mode=${1}
+[ -d ~/Desktop/minimize/${2} ] && distro=${2}
+
+case $# in
+	1)
+		dqb "maybe ok"
+	;;
+	2)
+		[ "${2}" == "-v" ] && debug=1
+	;;
+	3)
+		if [ "${2}" == "-v" ] ; then
+			debug=1
+			distro=${3}
+		else
+			[ "${3}" == "-v" ] && debug=1
+		fi
+	;;
+	*)
+		echo "${0} <mode> [other_params]";exit
+	;;
+esac
+
+dqb "mode=${mode}"
+dqb "distro=${distro}"
+csleep 2
+
+#HUOM. jos tarttee ni näille main distrosta riippuvainen fktioiden esittely 
+#(toiv ei tarvitse)
+
+smr=$(sudo which rm)
+ipt=$(sudo which iptables)
+slinky=$(sudo which ln)
+spc=$(sudo which cp)
+slinky="${slinky} -s "
+sco=$(sudo which chown)
+scm=$(sudo which chmod)	
+svm=$(sudo which mv)
+
+#240325 lisäykset
+ip6t=$(sudo which ip6tables)
+iptr=$(sudo which iptables-restore)
+ip6tr=$(sudo which ip6tables-restore)
+
+dqb "when in trouble, \"sudo chmod 0755 *.sh ;sudo chmod 0755 ${distro}; sudo chmod 0755 ${distro}/*.sh; sudo chmod 0644 ${distro}/conf\" may help "
+#==============================================================
+function tod_dda() { 
+	dqb "tod_dda(${1}) "
+	${ipt} -A b -p tcp --sport 853 -s ${1} -j c
+	${ipt} -A e -p tcp --dport 853 -d ${1} -j f
+}
+
+function dda_snd() {
+	dqb "dda_snd( ${1})"
+	${ipt} -A b -p udp -m udp -s ${1} --sport 53 -j ACCEPT 
+	${ipt} -A e -p udp -m udp -d ${1} --dport 53 -j ACCEPT
+}
+
+#HUOM.220624:stubbyn asentumisen ja käynnistymisen kannalta sleep saattaa olla tarpeen
+function ns2() {
+	[ y"${1}" == "y" ] && exit
+	dqb "ns2( ${1} )"
+	${scm} u+w /home
+	csleep 3
+
+	/usr/sbin/userdel ${1}
+	sleep 3
+
+	adduser --system ${1}
+	sleep 3
+
+	${scm} go-w /home
+	${sco} -R ${1}:65534 /home/${1}/ #HUOM.280125: tässä saattaa mennä metsään ... tai sitten se /r/s.pid
+	dqb "d0n3"
+	csleep 4	
+
+	[ ${debug} -eq 1 ]  && ls -las /home
+	csleep 3
+}
+
+function ns4() {
+	dqb "ns4( ${1} )"
+
+	${scm} u+w /run
+	touch /run/${1}.pid
+	${scm} 0600 /run/${1}.pid
+	${sco} ${1}:65534 /run/${1}.pid
+	${scm} u-w /run
+
+	sleep 5
+	${whack} ${1}* #saattaa joutua muuttamaan vielä
+	sleep 5
+
+	dqb "starting ${1} in 5 secs"
+
+	sleep 5
+	${odio} -u ${1} ${1} -g #antaa nyt tämän olla näin toistaiseksi(25.3.25)
+	echo $?
+
+	sleep 1
+	pgrep stubby*
+	sleep 5
+}
+
+function clouds_pp1() {
+	#c.pp.1
+	if [ -s /etc/resolv.conf.1 ] || [ -s /etc/resolv.conf.0 ] ; then 
+		${smr} /etc/resolv.conf
+		[ $? -gt 0 ] && echo "FAILURE TO COMPLY WHILE TRYING TO REMOVE RESOLV.CONF"
+	fi
+
+	if [ -s /etc/dhcp/dhclient.conf.1 ] || [ -s /etc/dhcp/dhclient.conf.0 ] ; then 
+		${smr} /etc/dhcp/dhclient.conf
+		[ $? -gt 0 ] && echo "FAILURE TO CPMPLY WHILÖE TRYING TO REMOVE DHCLIENT.CONF"
+	fi
+
+	if [ -s /sbin/dhclient-script.1 ] || [ -s /sbin/dhclient-script.0 ] ; then 	
+		${smr} /sbin/dhclient-script
+		[ $? -gt 0 ] && echo "FAILURE TO CPMPLY WHIOLE TRYINMG TO REMOVE DHCLIENT-SCRIPT"
+	fi
+}
+
+function clouds_pp2() {
+	dqb "#c.pp.2 ${1}"
+	csleep 1
+	
+	#HUOM. pitäisiköhän linkit hukata jokatapauksessa? 0<->1 - vaihdoissa silloin häviää linkitys kokonaan
+		
+	if [ -h /etc/iptables/rules.v4 ] ; then
+		dqb "smr /e/i/rv4"
+		${smr} /etc/iptables/rules.v4
+	else
+		${svm} /etc/iptables/rules.v4 /etc/iptables/rules.v4.OLD		
+	fi
+
+	if [ -h /etc/iptables/rules.v6 ] ; then
+		dqb "smr /e/i/r6v"
+		${smr} /etc/iptables/rules.v6
+	else
+		${svm} /etc/iptables/rules.v6 /etc/iptables/rules.v6.OLD
+	fi
+
+	if [ -s /etc/iptables/rules.v4.${1} ] ; then
+		${slinky} /etc/iptables/rules.v4.${1} /etc/iptables/rules.v4
+		dqb "stinky1"
+	fi
+
+	if [ -s /etc/iptables/rules.v6.${1} ] ; then
+		${slinky} /etc/iptables/rules.v6.${1} /etc/iptables/rules.v6
+		dqb "stunky2"
+	fi
+}
+
+function clouds_pp3() {
+	csleep 1
+	dqb "RELOADING TBLZ RULEZ"
+	csleep 1
+
+	#HUOM.160325:lisätty uutena varm. vuoksi
+	${iptr} /etc/iptables/rules.v4
+	${ip6tr} /etc/iptables/rules.v6
+	csleep 2
+
+	#tässä oikea paikka tables-muutoksille vai ei?
+	${ipt} -F b
+	${ipt} -F e
+
+	#pidemmän päälle olisi kätevämpi nimetä kuin numeroida ne säännöt...
+	${ipt} -D INPUT 5
+	${ipt} -D OUTPUT 6
+}
+
+function clouds_pre() {
+	dqb "cdns.clouds_pre()"
+	clouds_pp1
+	clouds_pp2 ${1}
+	clouds_pp3
+	dqb "... done"
+}
+
+function clouds_post() {
+	dqb "cdns.clouds_post() "
+	dqb "scm= ${scm}"
+	dqb "sco = ${sco}"
+	csleep 5
+
+	${scm} 0444 /etc/resolv.conf*
+	${sco} root:root /etc/resolv.conf*
+
+	${scm} 0444 /etc/dhcp/dhclient*
+	${sco} root:root /etc/dhcp/dhclient*
+	${scm} 0755 /etc/dhcp
+
+	${scm} 0555 /sbin/dhclient*
+	${sco} root:root /sbin/dhclient*
+	${scm} 0755 /sbin
+
+	${sco} -R root:root /etc/iptables
+	${scm} 0400 /etc/iptables/*
+	${scm} 0750 /etc/iptables
+	csleep 2
+
+	if [ ${debug} -eq 1 ] ; then
+		${ipt} -L  #
+		${ip6t} -L #parempi ajaa vain jos löytyy
+		csleep 3 #
+	fi #
+
+	dqb "d0n3"
+}
+
+function clouds_case0() {
+	dqb "cdns.clouds_case0()"
+
+	if [ y"${ipt}" == "y" ] ; then
+		dqb "SHOULD 1NSTALL TABL35"
+		exit 88
+	else
+		${ipt} -A INPUT -p udp -m udp --sport 53 -j b 
+		${ipt} -A OUTPUT -p udp -m udp --dport 53 -j e
+
+		if [ -s  /etc/resolv.conf ] ; then
+			for s in $(grep -v '#' /etc/resolv.conf | grep names | grep -v 127. | awk '{print $2}') ; do dda_snd ${s} ; done	
+		else
+			dqb "NO RESOLV.CONF FOUND, HAVE TO USE CONF"
+			csleep 1
+			
+			if [ z"${dsn}" != "z" ] ; then
+				for s in ${dsn} ;  do dda_snd ${s} ; done
+			fi
+		fi
+	fi
+
+	/etc/init.d/dnsmasq stop
+	/etc/init.d/ntpsec stop
+	csleep 3
+	${whack} dnsmasq*
+	${whack} ntp*
+}
+
+function clouds_case1() {
+	echo "WORK IN PROGRESS"
+
+	if [ y"${ipt}" == "y" ] ; then
+		echo "SHOULD 1NSTALL TABL35"
+	else
+		${ipt} -A INPUT -p tcp -m tcp --sport 853 -j b
+		${ipt} -A OUTPUT -p tcp -m tcp --dport 853 -j e
+
+		if [ -s /home/stubby/.stubby.yml ] ; then
+			for s in $(grep -v '#' /home/stubby/.stubby.yml | grep address_data | cut -d ':' -f 2) ; do tod_dda ${s} ; done
+		else
+			dqb "NO CONF FOUND, HAVE TO USE ALT CONF"
+			csleep 1
+		
+			if [ z"${dsn}" != "z" ] ; then
+				for s in ${dsn} ;  do tod_dda ${s} ; done
+			fi
+		fi
+	fi
+
+	echo "dns";sleep 2
+	/etc/init.d/dnsmasq restart
+	pgrep dnsmasq
+
+#HUOM.270325:tästä eteenpäin vaatinee pientä laittoa
+#ensinnäkin dnsmasq pitäisi saada taas vastaamaan pyyntöihin ja sitten muut jutut
+#	echo "stu";sleep 2
+#	${whack} stubby* #090325: pitäisiköhän tämä muuttaa?
+#	sleep 3	
+#			
+#	[ -f /run/stubby.pid ] || sudo touch /run/stubby.pid
+#	${sco} devuan:devuan /run/stubby.pid #$n
+#	${scm} 0644 /run/stubby.pid 
+#	sleep 3
+#
+#	su devuan -c '/usr/bin/stubby -C /home/stubby/.stubby.yml -g'
+#	pgrep stubby
+}
+#====================================================================
+clouds_pre ${mode}
+
+[ -f /etc/resolv.conf.${mode} ] && ${slinky} /etc/resolv.conf.${mode} /etc/resolv.conf
+[ -f /etc/dhcp/dhclient.conf.${mode} ] && ${slinky} /etc/dhcp/dhclient.conf.${mode} /etc/dhcp/dhclient.conf
+[ -f /sbin/dhclient-script.${mode} ] && ${spc} /sbin/dhclient-script.${mode} /sbin/dhclient-script
+
+case ${mode} in 
+	0)
+		clouds_case0
+	;;
+	1)
+		clouds_case1
+	;;
+	*)
+		echo "MEE HIMAAS LEIKKIMÄHÄN"
+	;;
+esac
+
+clouds_post
---
+++ generic_doit.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+mode=2
+distro=$(cat /etc/devuan_version) #voisi olla komentoriviparametrikin jatkossa?
+d=~/Desktop/minimize/${distro} 
+[ z"${distro}" == "z" ] && exit 6
+debug=0 #1
+
+if [ -d ${d} ] && [ -s ${d}/conf ]; then
+	. ${d}/conf
+else
+	echo "CONFIG MISSING"
+	exit 55
+fi
+
+function parse_opts_1() {
+	case "${1}" in
+		-v|--v)
+			debug=1
+		;;
+		*)
+			mode=${1}
+		;;
+	esac
+}
+
+. ~/Desktop/minimize/common_lib.sh
+#HUOM.140525:vaikuttaisi siltä että vasta lib.sh includoinnin jälkeen alkaa 076 pelata, kts toistuuko+koita korjata
+
+function parse_opts_2() {
+	#toistaiseksi oltava common_lib jälkeen koska dqb
+	dqb "parseopts_2 ${1} ${2}"
+}
+
+#VAIH:gpo():n kautta jatkossa, jos mahd (vaikuttaisi siltä että $@ ei kulkeudu automaagisesti, man bash+KVG)
+#HUOM.tämä blokki oli aiemmin juuri ennen pre_enforce():a
+if [ $# -gt 0 ] ; then
+	for opt in $@ ; do parse_opts_1 $opt ; done
+fi
+#check_params
+#gpo
+#exit
+
+dqb "b3f0r3 p.076"
+csleep 1
+part076
+#exit
+
+if [ -d ${d} ] && [ -x ${d}/lib.sh ] ; then
+	. ${d}/lib.sh
+else
+	echo "TO CONTINUE FURTHER IS POINTLESS, ESSENTIAL FILES MISSING"
+	exit 111
+fi
+
+function vommon() {
+	dqb "R (in 3 secs)"
+	csleep 3
+	${odio} passwd
+
+	#miksi tähän ei mennä? vai mennäänkö?
+	if [ $? -eq 0 ] ; then
+		dqb "L (in 3 secs)"
+		csleep 3
+		passwd
+	fi
+
+	if [ $? -eq 0 ] ; then
+		${whack} xfce4-session
+		#HUOM. tässä ei tartte jos myöhemmin joka tap
+	else
+		dqb "SHOULD NAG ABOUT WRONG PASSWD HERE"
+	fi
+
+	exit
+}
+
+#HUOM. onko chimaerassa oletuksena ntp-paketteja asnennettuna?
+function part1_post() {
+	#ntp ehkä takaisin myöhemmin
+	${whack} ntp*
+	csleep 5
+	${odio} /etc/init.d/ntpsec stop
+	#K01avahi-jutut sopivaan kohtaan?
+}
+
+#==================================PART 1============================================================
+
+dqb "mode= ${mode}"
+csleep 5
+#exit
+
+#HUOM.13525:pre_e:tä tarttisi ajaa vain kerran, jossain voisi huomioida /e/s.d/m olemassaolon
+[ ${enforce} -eq 1 ] && pre_enforce ${n} ${distro}
+enforce_access ${n}
+ 
+part1 ${distro} 
+#HUOM.190325:part_1_5sessa oli bugi, u+w ei vaan riitä
+[ ${mode} -eq 0 ] && exit
+
+${snt} #HUOM14525:oli tässä ennen: part175
+csleep 3
+part1_post #tarpeellinen jatkossa? josko copy-paste kuitenkin
+
+#jotain perusteellisempia testejä chimaeran kanssa sitten mikäli jksaa sitä kirjautumisongelmaa
+#===================================================PART 2===================================
+
+c14=0
+[ ${mode} -eq 1 ] && c14=1
+c13=$(grep -v '#' /etc/default/locale  | grep LC_TIME | grep -c ${LCF666})
+[ ${c13} -lt 1 ] && c14=1
+
+el_loco ${c14} ${c13}
+#HUOM.15525:joskohan el_loco toimisi jo kuten tarkoitus?
+
+if [ ${mode} -eq 1 ] || [ ${changepw} -eq 1 ] ; then 
+	vommon
+	exit #varm. vuoksi kesk. suor. jos salakala tyritty
+fi
+
+c14=$(find ${d} -name '*.deb' | wc -l)
+[ ${c14} -gt 0 ] || removepkgs=0
+
+part2_pre ${removepkgs}
+#part2 ${removepkgs} #takaisin jos 2_5 pykii
+part2_5 ${removepkgs}
+
+#===================================================PART 3===========================================================
+message #voi muuttua turhaksi jatkossa
+
+#HUOM.10525:jossain näillä main oli nalkutusta, ilmeinen korjaus tehty
+pre_part3 ${d}
+pr4 ${d}
+part3 ${d}
+
+[ -s ~/Desktop/minimize/xfce.tar ] && ${srat} -C / -xf ~/Desktop/minimize/xfce.tar
+csleep 5
+
+#tai sitten käskytetään:import2 (jatkossa -> part3_post ?)
+if [ -x ~/Desktop/minimize/profs.sh ] ; then
+	. ~/Desktop/minimize/profs.sh
+
+	q=$(mktemp -d)
+	dqb "${srat} -C ${q} ... 1n 3 s3c5s"
+	csleep 3
+	tgt=~/Desktop/minimize/fediverse.tar
+
+	if [ -s ${tgt} ] ; then	
+		${srat} -C ${q} -xvf ${tgt}
+
+		dqb "${srat} d0me"
+		csleep 3
+
+		imp_prof esr ${n} ${q}
+	else
+		dqb "NO SUCH THING AS ${tgt}"
+	fi
+
+	csleep 3
+fi
+
+jules
+${asy}
+dqb "GR1DN BELIALAS KYE"
+
+${scm} 0555 ~/Desktop/minimize/changedns.sh
+${sco} root:root ~/Desktop/minimize/changedns.sh
+${odio} ~/Desktop/minimize/changedns.sh ${dnsm} ${distro}
+${sipt} -L
+csleep 6
+
+${scm} a-wx $0
+#===================================================PART 4(final)==========================================================
+
+#HUOM.12525:tämä kohta ei vaikuttanut toimivan kunnolla, toi bttavasti tilapäist
+
+if [ ${mode} -eq 2 ] ; then
+	echo "time to ${sifu} ${iface} or whåtever"
+	csleep 2
+	${whack} xfce4-session
+ 	exit 
+fi
+
+${odio} ~/Desktop/minimize/changedns.sh ${dnsm} ${distro}
