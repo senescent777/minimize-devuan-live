378ae9f54bef8fd13b6f53221baa0b4f47eb1d08
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-15 14:36:50 +0300

    after tests 2

--- minimize/chimaera/lib.sh
+++ minimize/chimaera/lib.sh
@@ -1,20 +1,24 @@
-#=========================PART 0 ENDS HERE=================================================================
 function pr4() {
+	dqb "pre4 ${1} ${2}"
+	csleep 2
 	psqa ${1}
 	#TODO:dnsm-jutuille jos tekisijojo tain , esim. stubby+dnsmasq toimimaan ni ei tartte noita poistella
 
-	if [ ${dnsm} -eq 1 ] ; then #TODO:dnsm paraetriksi
+	#if [ ${dnsm} -eq 1 ] ; then #VAIH:dnsm paraetriksi
+	if [ ${2} -eq 1 ] ; then
 		${NKVD} ${1}/stubby*
 		${NKVD} ${1}/libgetdns*
 		${NKVD} ${1}/dnsmasq*
 	fi
+
+	csleep 1
 }
 
 function pre_part3() {
-	dqb "PER3"
+	dqb "PER3 ${1} ${2}"
 	csleep 1
 
-	if [ ${dnsm} -eq 1 ] ; then #TODO:dnsm parametriksi
+	if [ ${2} -eq 1 ] ; then #VAIH:dnsm parametriksi
 		${sdi} ${1}/dns-root-data*.deb
 		${NKVD} ${1}/dns-root-data*.deb
 	fi
