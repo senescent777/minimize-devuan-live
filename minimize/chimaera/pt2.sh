531f2944d2aff84349b8778248a6017f68ef5640
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-23 22:04:41 +0300

    Add files via upload

--- minimize/chimaera/pt2.sh
+++ minimize/chimaera/pt2.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-
 d=$(dirname $0)
 [ -s ${d}/conf ] && . ${d}/conf
+
+function parse_opts_1() {
+	case "${1}" in
+		-v|--v)
+			debug=1
+		;;
+	esac
+}
+
+function parse_opts_2() {
+	dqb "parseopts_2 ${1} ${2}"
+}
+
 . ~/Desktop/minimize/common_lib.sh
 [ -s ${d}/lib.sh ] && . ${d}/lib.sh
 
@@ -9,13 +21,15 @@ ${scm} 0555 ~/Desktop/minimize/changedns.sh
 ${sco} root:root ~/Desktop/minimize/changedns.sh
 ${fib}
 
-if [ $# -gt 0 ] ; then  
-	if [ "${1}" == "-v" ] ; then
-		debug=1
-	fi
-fi
+#if [ $# -gt 0 ] ; then  
+#	if [ "${1}" == "-v" ] ; then
+#		debug=1
+#	fi
+#fi
+#
+dqb "a-e"
+csleep 5
 
-#VAIH:pitäisi kai miettiä tämä kohta, ideana doit-skriptin puolella kai että ei poisteta iptables jos ei korvaavaa pakerria
 if [ ${removepkgs} -eq 1 ] ; then
 	dqb "kö"
 else
@@ -23,154 +37,106 @@ else
 	part2_5 1 #HUOM.15425: oli part2 1
 fi
 
-${odio} which iptables; csleep 3
+#libgutls-dane, aiheuttaako härdelliä jos poistaa? ei näköjään
 
-#==============================================================
-#HUOM! PAKETIT procps, mtools JA mawk JÄTETTÄVÄ RAUHAAN!!!
-#==============================================================
-${odio} which iptables; csleep 3
+#HUOM. ao. rivillä 2. viimeisessä syystä vain core
+${sharpy} amd64-microcode iucode-tool arch-test at-spi2-core 
+${sharpy} bubblewrap atril* coinor* cryptsetup* debootstrap
 
+#HUOM.23525:poistettavien listaa pystyisi ehkä säätämään vielä, dpkg -l
+
+${asy} 
 ${lftr}
-${fib}
-${sharpy} amd64-microcode atril* at-spi2-core coinor*
+csleep 5
+
+${sharpy} dmidecode discover* dirmngr #tuleekohan viimeisestä ongelma? vissiin ei
+${sharpy} doc-debian docutils* efibootmgr exfalso 
+${sharpy} fdisk ftp* gdisk gcr
+
+${asy} 
+${lftr}
+csleep 5
+
+##gnome-* poisto veisi myös: task-desktop task-xfce-desktop
+##gpg* kanssa: The following packages have unmet dependencies:
+## apt : Depends: gpgv but it is not going to be installed or
+##                gpgv2 but it is not going to be installed or
+##                gpgv1 but it is not going to be installed
+##HUOM. grub* poisto voi johtaa shim-pakettien päivitykseen
+##gsettings* voi viedä paljon paketteja mukanaan
+
+dqb "g2"
+csleep 5
+
+${sharpy} ghostscript gir* gnupg* gpg-*
+${sharpy} gpgconf gpgsm gsasl-common shim*
+${sharpy} grub* gsfonts gstreamer*
+${sharpy} intel-microcode iucode-tool htop inetutils-telnet
+
+${asy} 
+${lftr}
+csleep 5
+
+#lib-paketteihin ei yleisessä tapauksessa kande koskea eikä live-
+#libgssapi-krb5 tarpeellinen?
+#HUOM! PAKETIT procps, mtools JA mawk JÄTETTÄVÄ RAUHAAN!!! (tai mtools ehkä uskaltaa kun o muitsa poistettu ensin)
+
+#HUOM.15525:uutena libcolor, näytti poistavan liikaa joten jemmaan
+#${sharpy} libcolor* 
+#csleep 5
+
+${sharpy} libpoppler* libuno* libreoffice* libgsm* libgstreamer*
+${sharpy} lvm2 lynx* mdadm mailcap
+
+${asy} 
+${lftr}
+csleep 5
+
+${sharpy} mlocate mokutil mariadb-common mysql-common
+${sharpy} netcat-traditional openssh* os-prober #orca saattaa poistua jo aiemmin
+${sharpy} nfs-common rpcbind
+
+csleep 5
+dqb "p"
+csleep 5
+
+${sharpy} ppp procmail ristretto screen
+${sharpy} pkexec po* refracta* squashfs-tools
+
+${asy} 
+${lftr}
+csleep 5
+
+#HUOM.15525 vaikuttaisi toimivan, ajon jälkeen x toimii edelleen
+${sharpy} samba* system-config* telnet tex* 
+
+#VAIH:, python3*, , voisiko niitä karsia?
+${sharpy} syslinux* mythes isolinux libgssapi-krb5-2
+
+${sharpy} uno* ure* upower vim* # udisks* saattaa poistua jo aiemmin
+${asy} 
+${lftr}
+csleep 5
+
+dqb "x"
+csleep 5
+${sharpy} xorriso xfburn
 ${asy} 
 
 ${lftr}
-${fib}
-${sharpy} dirmngr discover* distro-info-data efibootmgr exfalso ftp gcr
-${asy}
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${fib}
-${sharpy} ghostscript gir* gdisk gpg-* gpgconf gpgsm gparted
-${asy}
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${fib}
-${sharpy} gsasl* gsfonts* gstreamer*
-${asy}
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3 #tulostuksetkin dbg taakse?
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${fib}
-${sharpy} htop 
-${asy}
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-csleep 5
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${fib}
-${sharpy} intel-microcode
-${asy}
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-csleep 5
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${sharpy} mdadm 
-${asy}
-csleep 6
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-csleep 5
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${sharpy} lvm2
-${asy}
-csleep 6
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-csleep 5
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${sharpy} mailcap mariadb-common
-${asy}
-${lftr}
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-${fib}
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${sharpy} mokutil mysql-common orca openssh*
-${asy}
-${lftr}
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${sharpy} speech* system-config* telnet tex* udisks2 uno* ure* upower
-${sa} autoremove --yes
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-${fib}
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${sharpy} vim* xorriso xfburn
-${asy}
-
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${sharpy} iucode-tool libgstreamer* os-prober po*
-${asy}
-
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
-${fib}
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${sharpy} ppp 
-${asy}
-csleep 1
-
-${lftr}
-${sharpy} ristretto
-${asy}
-csleep 1
-
-${sharpy} screen shim* samba* 
-${asy}
-csleep 1
-
-[ ${debug} -eq 1 ] && ${odio} which iptables; csleep 3
-
-${lftr}
-${sharpy} procmail
-${asy}
-csleep 1
-
-${lftr}
-${sharpy} squashfs-tools
-${asy}
-csleep 6
-
-${lftr}
-${sharpy} grub*
-${asy}
-csleep 6
-
-${lftr}
-${sharpy} libgsm*
-${asy} 
-csleep 6
-
-${lftr}
-${NKVD} /var/cache/apt/archives/*.deb
+${NKVD} ${pkgdir}/*.deb
+${NKVD} ${pkgdir}/*.bin 
 ${NKVD} ${d}/*.deb 
-${NKVD} /tmp/*.tar 
+${NKVD} /tmp/*.tar
 ${smr} -rf /tmp/tmp.*
-${smr} /usr/share/doc 
+${smr} /usr/share/doc #rikkookohan jotain nykyään? (vuonna 2005 ei rikkonut)
+#squ.ash voisi vilkaista kanssa liittyen (vai oliko mitään hyödyllistä siellä vielä?)
 df
+${odio} which dhclient; ${odio} which ifup; csleep 6
 
-[ ${debug} -eq 1 ] && ${odio} which dhclient; ${odio} which ifup; csleep 3
+dqb "${scm} a-wx $0 in 6 secs "
+csleep 6
+${scm} a-wx $0 
+
+#whack xfce so that the ui is reset
+${whack} xfce4-session
