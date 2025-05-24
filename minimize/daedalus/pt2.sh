6a31c7b7c8807d6fb3e86e8e863fb1c883cd93f6
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-24 14:17:06 +0300

    returned earlier version from main-branch

--- minimize/daedalus/pt2.sh
+++ minimize/daedalus/pt2.sh
@@ -23,8 +23,7 @@ ${sco} root:root ~/Desktop/minimize/changedns.sh
 ${fib}
 
 dqb "a-e"
-csleep 2
-${fib} #HUOM.23525:oli kiukuttelua, josko tällä asettuisi
+csleep 5
 
 if [ ${removepkgs} -eq 1 ] ; then
 	dqb "kö"
@@ -44,7 +43,7 @@ ${sharpy} fdisk ftp* gdisk gcr
 
 ${asy} 
 ${lftr}
-csleep 2
+csleep 5
 
 ##gnome-* poisto veisi myös: task-desktop task-xfce-desktop
 ##gpg* kanssa: The following packages have unmet dependencies:
@@ -55,113 +54,53 @@ csleep 2
 ##gsettings* voi viedä paljon paketteja mukanaan
 
 dqb "g2"
-csleep 2
+csleep 5
 
 ${sharpy} ghostscript gir* gnupg* gpg-*
 ${sharpy} gpgconf gpgsm gsasl-common shim*
 ${sharpy} grub* gsfonts gstreamer*
-${sharpy} intel-microcode iucode-tool
-${asy} 
-
-${sharpy} htop
-${asy} 
-${sharpy} inetutils-telnet
-${asy} 
+${sharpy} intel-microcode iucode-tool htop inetutils-telnet
 
 ${asy} 
 ${lftr}
-csleep 2
+csleep 5
 
 #lib-paketteihin ei yleisessä tapauksessa kande koskea eikä live-
 #libgssapi-krb5 tarpeellinen?
 #HUOM! PAKETIT procps, mtools JA mawk JÄTETTÄVÄ RAUHAAN!!! (tai mtools ehkä uskaltaa kun o muitsa poistettu ensin)
 #HUOM.15525:uutena libcolor, näytti poistavan liikaa joten jemmaan
 #${sharpy} libcolor* 
-#csleep 2
+#csleep 5
 
 ${sharpy} libpoppler* libuno* libreoffice* libgsm* libgstreamer*
+${sharpy} lvm2 lynx* mdadm mailcap
+
 ${asy} 
 ${lftr}
+csleep 5
 
-#josko nyt poistuisi prkl?
-${sharpy} lvm2 
-${asy}
-
-${sharpy} lynx* 
-${asy}
-
-${sharpy} mdadm 
-${asy}
-
-${sharpy} mailcap
-${asy} 
-${lftr}
-csleep 2
-
-${sharpy} mlocate 
-${asy} 
-${sharpy} mokutil mariadb-common mysql-common
-${asy} 
-
-${sharpy} netcat-traditional 
-${asy} 
-${sharpy} openssh* 
-${asy} 
-${sharpy} os-prober #orca saattaa poistua jo aiemmin
-${asy} 
-
+${sharpy} mlocate mokutil mariadb-common mysql-common
+${sharpy} netcat-traditional openssh* os-prober #orca saattaa poistua jo aiemmin
 ${sharpy} nfs-common rpcbind
 ${sharpy} ppp procmail ristretto screen
-${sharpy} pkexec po* 
-${asy} 
-
-${sharpy} refracta*
-${asy} 
-${sharpy} squashfs-tools
-${asy} 
-${sharpy} pavucontrol #HUOM.22525:näyttäisi poistuvan ilman katastrofia, pieni nalkutus kyllä tulee mutta ei välttämättä juuri tuosta paketista
+${sharpy} pkexec po* refracta* squashfs-tools
 
 ${asy} 
 ${lftr}
-csleep 2
+csleep 5
 
 #VAIH:, python3*, , voisiko niitä karsia?
-${sharpy} samba* system-config*
-${asy} 
-${sharpy} telnet
-${asy} 
-${sharpy} tex* 
-${asy} 
-csleep 2
-
-${sharpy} syslinux* mythes
-${asy} 
-${sharpy} isolinux 
-${asy} 
-${sharpy} libgssapi-krb5-2
-${asy}
-csleep 2
-${sharpy} uno* ure* upower 
-${asy} 
-
-${sharpy} vim* # udisks* saattaa poistua jo aiemmin
+${sharpy} samba* system-config* telnet tex* 
+${sharpy} syslinux* mythes isolinux libgssapi-krb5-2
+${sharpy} uno* ure* upower vim* # udisks* saattaa poistua jo aiemmin
 
 ${asy} 
 ${lftr}
-csleep 2
+csleep 5
 
 dqb "x"
-csleep 2
-${sharpy} xorriso
-${asy} 
-${sharpy} xfburn
-${asy} 
-
-csleep 2
-
-${sharpy} xorriso
-${asy} 
-${sharpy} xfburn
+csleep 5
+${sharpy} xorriso xfburn
 ${asy} 
 
 ${lftr}
@@ -173,10 +112,10 @@ ${smr} -rf /tmp/tmp.*
 ${smr} /usr/share/doc #rikkookohan jotain nykyään? (vuonna 2005 ei rikkonut)
 #squ.ash voisi vilkaista kanssa liittyen (vai oliko mitään hyödyllistä siellä vielä?)
 df
-${odio} which dhclient; ${odio} which ifup; csleep 2
+${odio} which dhclient; ${odio} which ifup; csleep 6
 
 dqb "${scm} a-wx $0 in 6 secs "
-csleep 2
+csleep 6
 ${scm} a-wx $0 
 
 #whack xfce so that the ui is reset
