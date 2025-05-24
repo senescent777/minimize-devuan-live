ce7f8215113166b96b0807de3be49a5c6621558c
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-25 02:13:43 +0300

    a new directory

---
+++ minimize/excalibur/lib.sh
@@ -0,0 +1,29 @@
+function pre_part3() {
+	dqb "xc.pp3( ${1} , ${2} )"
+	csleep 1
+	#TODO
+}
+
+function pr4() {
+	dqb "xc.pr4( ${1} , ${2} )"
+	csleep 1
+	#TODO
+}
+
+function udp6() {
+	dqb "xc.lib.UPDP-6"
+	csleep 1
+	#TODO
+}
+
+function part2_pre() {
+	dqb "xc.PP2"
+	#TODO
+}
+
+function t2p() {
+	dqb "TODO"
+}
+
+check_binaries ${PREFIX}/${distro}
+check_binaries2
