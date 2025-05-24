0f1d72f5a9237a1de48eea44863697e5f2a5c5c7
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-24 21:58:02 +0300

    Add files via upload

--- minimize/generic_pt2.sh
+++ minimize/generic_pt2.sh
@@ -1,36 +1,60 @@
 #!/bin/bash
 distro=$(cat /etc/devuan_version)
-debug=0 #1
+debug=1 #HUOM.24525:pientä laittoa vielä komentorivioptioiden kanssa
 d="" #$(dirname $0)
 loose=1
 PREFIX=~/Desktop/minimize #josqo tähän se dirname?
 
+#HUOM.24525:meneekö -v perille asti? erit. wrapperin kautta? let's find out?
+
+function dqb() {
+	[ ${debug} -eq 1 ] && echo ${1}
+}
+
+function csleep() {
+	[ ${debug} -eq 1 ] && sleep ${1}
+}
+
 function parse_opts_1() {
-	case "${1}" in
+	echo "gp2.po1( ${1} )"
+
+	case ${1} in
 		-v|--v)
 			debug=1
 		;;
-		#tarvisiko tähän sen distron asettamisen?
 	esac
+
+	#tarvisiko yo.caseen sen distron asettamisen?
 }
 
 function parse_opts_2() {
-	dqb "parseopts_2 ${1} ${2}"
+	dqb "gp2.parseopts_2 ${1} ${2}"
 }
 
+if [ -x ${PREFIX}/common_lib.sh ] ; then
+	. ${PREFIX}/common_lib.sh
+	[ $? -gt 0 ] && exit 7
+else
+	echo "COMMON LIB NOT FOUND"
+	exit 11
+fi
 
-. ${PREFIX}/common_lib.sh
-[ $? -gt 0 ] && exit 7
 [ -z ${distro} ] && exit 8
 d=${PREFIX}/${distro}
- 
+#jos ei muuten lähde niin kopsaa jutut sieltä mistä toimii tai korjoita tyhjästä uusiksi koko paska
 [ -d ${d} ] || exit 9
 [ -s ${d}/conf ] && . ${d}/conf
-[ -s ${d}/lib.sh ] && . ${d}/lib.sh #mihin tuota taas tarvittiinkaan? kutsumaan check_bin varmaankin
+[ -x ${d}/lib.sh ] && . ${d}/lib.sh #mihin tuota taas tarvittiinkaan? kutsumaan check_bin varmaankin
 
 ${scm} 0555 ${PREFIX}/changedns.sh
 ${sco} root:root ${PREFIX}/changedns.sh
 ${fib}
+
+dqb "d=${d}"
+echo "debug=${debug}"
+dqb "distro=${distro}"
+dqb "removepkgs=${removepkgs}"
+#exit 10
 
 dqb "a-e"
 csleep 2
