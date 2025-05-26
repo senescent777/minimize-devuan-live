244dc5dd7169f507e5ec246fc976d4cc53ef5b36
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-26 16:46:49 +0300

    Add files via upload

--- minimize/chimaera/lib.sh
+++ minimize/chimaera/lib.sh
@@ -1,80 +1,77 @@
 #=================================================PART 0=====================================
-
-#TEHTY:man dpkg, man apt, josqo saisi pakotettua sen vastauksen... tai ensin https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
+#
+#TEHTY?:man dpkg, man apt, josqo saisi pakotettua sen vastauksen... tai ensin https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
 #... joskohan --force-confold olisi se haettu juttu
+#HUOM.24525: jnkn verra n testailtu
 
-#HUOM. tarvitseeko 2. parametrin? ei kai
-#TODO:josko niin päin että ladataan rulekset ensin ja sitten asennetaan persistentit? onnistuisiko välttää cp:n takominen?
 function pre_part3() {
-	dqb "daud.pp3( ${1} , ${2} )"
+	dqb "ch1m.pp3( ${1} , ${2} )"
 	csleep 1
 
 	[ y"${1}" == "y" ] && exit	
 	[ -d ${1} ] || exit
 	dqb "pp3.2"
-
 	csleep 1
-	psqa ${1} #HUOM.22525:varm. vuoksi pp3 kutsuvaan koodiin tämä?
-	
-	#HUOM.140525:toiminee jo jollain lailla, "no" siihen kysymykseen olisi kuitenkin kiva saada välitettyä dpkg:lle asti
-	#HUOM.26525:jotain nalkutusta oli asentelujen yhteydess eli oikeuksia pitäisi kai loiventaa (TODO)
+
+	psqa ${1}
+	#HUOM.24525:vaikuttaisi toimivan vähän eri tavalla sääntjen tallennuksen sihteen q daedalus
+	#löytyi rules.v4 minkä sisältönä kaikkiin ketjuihin DROP (no vähän patrempi qu ACCEPT)
 
 	${odio} DEBIAN_FRONTEND=noninteractive dpkg --force-confold -i ${1}/netfilter-persistent*.deb
 	[ $? -eq 0 ] && ${NKVD} -f ${1}/netfilter-persistent*.deb
+	#csleep 2
 
-	#HUOM.26525:ylempänä mainittuun ideaan liittyen asentamisen järjestystä tulisi muuttaa, selvitä onnistuuko (TODO)
 	${odio} DEBIAN_FRONTEND=noninteractive dpkg --force-confold -i ${1}/libip*.deb
 	[ $? -eq 0 ] && ${NKVD} -f ${1}/libip*.deb
+	#csleep 2
 
 	${odio} DEBIAN_FRONTEND=noninteractive dpkg --force-confold -i ${1}/iptables_*.deb
 	[ $? -eq 0 ] && ${NKVD} -f ${1}/iptables_*.deb
+	#csleep 2
 
 	${odio} DEBIAN_FRONTEND=noninteractive dpkg --force-confold -i ${1}/iptables-*.deb
 	[ $? -eq 0 ] && ${NKVD} -f ${1}/iptables-*.deb
-
 	csleep 1
 
 	${scm} 0755 /etc/iptables
 	${svm} /etc/iptables/rules.v4 /etc/iptables.rules.v4.$(date +%F)
 	${svm} /etc/iptables/rules.v6 /etc/iptables.rules.v6.$(date +%F)
-	${scm} 0550 /etc/iptables	
+	${scm} 0550 /etc/iptables
 
 	dqb "pp3 d0n3"
-	csleep 1
+	csleep 2
 }
 
-#HUOM.19525:pitäisiköhän tässäkin olla se debian_froNtend-juttu? ehkä ei ole pakko
-#HUOM.26525:2. parametri, tartteeko moista?
 function pr4() {
-	dqb "daud.pr4( ${1} , ${2} )"
-	csleep 1
+	dqb "ch1m.pr4( ${1} , ${2} )"
+	csleep 3
 
-	#TODO:tähänkin psqa?
-	${sdi} ${1}/libpam-modules-bin_*.deb
-	${sdi} ${1}/libpam-modules_*.deb
+	${odio} dpkg -i ${1}/libpam-modules-bin_*.deb
+	${odio} dpkg -i ${1}/libpam-modules_*.deb
 
 	${NKVD} ${1}/libpam-modules*
+	csleep 1
 
-	${sdi} ${1}/libpam*.deb
-	${sdi} ${1}/perl-modules-*.deb
-	${sdi} ${1}/libperl*.deb
+	${odio} dpkg -i ${1}/libpam*.deb
+	${odio} dpkg -i ${1}/perl-modules-*.deb
+	${odio} dpkg -i ${1}/libperl*.deb
 
 	${NKVD} ${1}/perl-modules-*.deb
 	${NKVD} ${1}/libperl*.deb
+	csleep 1
 
-	${sdi} ${1}/perl*.deb
-	${sdi} ${1}/libdbus*.deb
-	${sdi} ${1}/dbus*.deb
+	${odio} dpkg -i ${1}/perl*.deb
+	${odio} dpkg -i ${1}/libdbus*.deb
+	${odio} dpkg -i ${1}/dbus*.deb
+	csleep 1
 
-	${sdi} ${1}/liberror-perl*.deb
-	${sdi} ${1}/git*.deb
+	${odio} dpkg -i ${1}/liberror-perl*.deb
+	${odio} dpkg -i ${1}/git*.deb
 
 	${NKVD} ${1}/git*.deb
 	${NKVD} ${1}/liberror-perl*.deb
 	csleep 1
-
-	#TODO:libdevmappwr-juttuja? tai mitä olikaan
 
 	${NKVD} ${1}/libpam*
 	${NKVD} ${1}/libperl*
@@ -85,8 +82,8 @@ function pr4() {
 }
 
 function udp6() {
-	dqb "daud.lib.UPDP-6"
-	csleep 1
+	dqb "ch1m.lib.UPDP-6"
+	csleep 2
 
 	${NKVD} ${pkgdir}/libx11-xcb1*
 	${NKVD} ${pkgdir}/nfs*
@@ -99,111 +96,111 @@ function udp6() {
 	${NKVD} ${pkgdir}/librsvg2*
 	
 	for s in ${PART175_LIST} ; do
-		dqb "processing ${s} ..."
+		dqb "processing ${s}"
 		csleep 1
 
 		${NKVD} ${pkgdir}/${s}*
+		csleep 1
 	done
 
 	case ${iface} in
 		wlan0)
 			dqb "NOT REMOVING WPASUPPLICANT"
-			csleep 1
+			csleep 6
 		;;
 		*)
-			${smr} ${pkgdir}/wpa*
+			${NKVD} ${pkgdir}/wpa*
 		;;
 	esac
 
 	dqb "D0NE"
-	csleep 1
+	csleep 4
 }
 
 #tarpeellinen fktio?
 function part2_pre() {
-	dqb "dausd.PP2"
-}
-
-#TODO:squashfs-tools vai tartteeko vielä?
-#https://pkginfo.devuan.org/cgi-bin/package-query.html?c=package&q=xcvt=0.1.2-1
-function t2p() {
-	debug=1
-	dqb "DAUD.T2P()"
-	csleep 3
-
-	#voisi kai chim kanssa yhteisiä viedä part2_5:seen`?
-	#HUOM.25525:atril ei löydy daedaluksesta
-
-	#bluez ei löydy, bc taisi poistua aiemmin
-
-	#doc-paketteja saattaisi vaikka tarvitakin mutta
-
-	#exfatprogs, tarvitseeko?
-
-	#gdisk, ghostscript, gnupg* ei löydy
-	#gpg* voi poistaa liikaa
-
-	#gparted, gpg-wks* ei löydy, gpgconf EI poistoon
-
-	#gsasl-common, gsfonts, gvfs ei löydy
-	${sharpy} arch-test
-	${sharpy} gsettings* #uskaltaako poistaa chimaerassa?
-	t2p_filler
-
-	${sharpy} iucode-tool #löytyykö chimaerasta?
-	t2p_filler
-
-	#jos lib* jättäisi enimmäkseeen rauhaan
-
-	#linux* , live* off limits
-	#mariadb-common ei löytynyt
-
-	#lp-solve? mysql-common?
-
-	#mawk off limits, mdadm ei löytynyt, mokutil ei, mobile ei, mutt ei, mysql ei
-
-	#node* ei löydy, notification* ei löydy, orca ei, os-prober ei
-	${sharpy} ntfs-3g #tilassa rc
-	t2p_filler
-
-	#pigz ei löydy, packagekit ei löydy
-	${sharpy} p7zip
-	t2p_filler
-
-	#pinentry ei, po* ei
-
-	#procps off limits
-	${sharpy} psmisc
-	t2p_filler
-
-	#python* parempi jättää rauhaan
-	#quodlibet ei löydy, refracta* ei
-
-	${sharpy} rsync
-	#löytyykö rpcbind?
-	t2p_filler
-
-	#sane-utils ei löydy
-	#löytyykö squash?
-
-	#telnet e löydy
-	${sharpy} traceroute
-	t2p_filler
-
-	#udisks2 ei löydy, uno ei löydy, ure ei
-	${sharpy} upower 
-	t2p_filler
-
-	${sharpy} w3m wget
-	#xfce*,xorg* off limits, xfburn ei löydy
-	${sharpy} xarchiver
-	t2p_filler
-
-	dqb "D0N3"
+	dqb "c.PP2"
 	csleep 1
 }
 
+function t2p() {
+	#t2pc #jatkossa kutsiva koodi ajamaan tämän
+	csleep 5
+
+	dqb "t2p()"
+	csleep 1
+
+	${sharpy} atril* #daedaluksess poistui aiemmin
+	${lftr}
+	${sharpy} bc bluez #daed
+	#bluezin saisi aikaisemminkin pois, art2_5...
+	t2p_filler
+
+	#dconf,debconf off limits
+
+	#HUOM. miten gdisk, löytyykö?
+	${sharpy} ghostscript #ei daed
+
+	#gnome* , gpg* off limits , gnupg* ei löydy
+
+	${sharpy} gparted* #daud ei löydy
+	t2p_filler
+	
+	#gsettings-desktop-schemas off limits
+	${sharpy} gsasl-common #eilöydy d
+	${sharpy} gsfonts* #eilöydy
+	t2p_filler
+
+	${sharpy} gvfs* #eilöydy
+	t2p_filler
+	
+	#löytyykö chimaerasta iucode-tool?
+
+	#lib-jutut jos antaisi enimmäkseen olla rauhassa
+	
+	#linux* , live* off limits
+	#lp-solve ei löytynyt?	
+	#${sharpy} mariadb-common #ei löytynyt d
+
+	${sharpy} mdadm # ei d
+	${sharpy} mobile* #ei d
+	t2p_filler
+
+	${sharpy} mysql-common #ei d?
+	t2p_filler
+
+	#ntfs-3g?
+	${sharpy} notification-daemon #ei löydy d
+	t2p_filler
+
+	${sharpy} orca packagekit* #ei löydy d
+	#{sharpy} p7zip ?
+	t2p_filler
+
+	#löytyykö plocate?
+	${sharpy} pigz #ei löydy d
+	${sharpy} po* #ei löydy d
+	t2p_filler
+
+	#procps off limits
+	#löytyykö psmisc? entä rsync?
+	${sharpy} rpcbind #miten rsync?
+	${sharpy} sane-utils #ei löydy d
+	t2p_filler
+
+	${sharpy} squash* #löytyykö d?
+	t2p_filler
+
+	#traceroute?
+	${sharpy} telnet #tartteeko erikseen? olisi se inetutils-telnet myös
+	t2p_filler
+
+	#upower? w3m? wget? xarchiver?
+	#xfce*,xorg* off limits
+
+	dqb "D0N3"
+	csleep 1
+} 
+
 check_binaries ${PREFIX}/${distro}
 check_binaries2
-
-
