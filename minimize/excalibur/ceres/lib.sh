274bba187a82fc23ce089e35da69d48a990f21c2
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-27 23:20:48 +0300

    after.3

--- minimize/excalibur/ceres/lib.sh
+++ minimize/excalibur/ceres/lib.sh
@@ -8,9 +8,21 @@ function pre_part3() {
 
 	#VAIH
 	sudo dpkg -i ~/Desktop/minimize/daedalus/libip*.deb
-	sudo dpkg -i ~/Desktop/minimize/daedalus/daedalus/libxtables12_1.8.9-2_amd64.deb 
-	sudo dpkg -i ~/Desktop/minimize/daedalus/daedalus/iptables_*.deb
-	#kunnes saa export2 toimimaan 
+	sudo rm ~/Desktop/minimize/daedalus/libip*.deb
+	csleep 1
+
+	#TODO:se efk()
+	sudo dpkg -i ~/Desktop/minimize/daedalus/libxtables12_1.8.9-2_amd64.deb 
+	sudo rm ~/Desktop/minimize/daedalus/libxtables12_1.8.9-2_amd64.deb 
+	csleep 1
+
+	sudo dpkg -i ~/Desktop/minimize/daedalus/iptables_*.deb
+	sudo rm ~/Desktop/minimize/daedalus/iptables_*.deb
+	csleep 1
+
+	#...kunnes saa export2 toimimaan
+	dqb "xc.pp3.d0n3()"
+	csleep 1
 }
 
 function pr4() {
@@ -32,8 +44,32 @@ function part2_pre() {
 	#TODO
 }
 
+#joskohan näiden jälkeen on excalibur vielä hengissä?
 function t2p() {
-	dqb "TODO"
+	debug=1
+
+	dqb "VAIH"
+	${sharpy} arch-test debootstrap dmsetup dracut-install
+	t2p_filler
+
+	${sharpy} fuse3 gir* grub* gsettings* gstreamer*
+	t2p_filler
+
+	#mitä tekee luit? entä libngtcp2? ocl-icd-jotain ?
+	${sharpy} libgstreamer* libgssapi* lm-sensors 
+	t2p_filler
+
+	${sharpy} mc* mythes* os-prober rsync
+	t2p_filler
+
+	${sharpy} squashfs-tools upower w3m wget
+	t2p_filler
+
+	${sharpy} xarchiver xfburn xorriso xz-utils #TODO:näitä sinne t2pc():hen jos ei...
+	t2p_filler
+
+	dqb "XC.T2PC.DONE"
+	csleep 1
 }
 
 check_binaries ${PREFIX}/${distro}
