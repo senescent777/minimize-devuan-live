8a57d018e67c286090aafc0c61bce58e6dfe72ac
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-27 20:39:35 +0300

    after testing w/ excalibur

--- minimize/excalibur/ceres/lib.sh
+++ minimize/excalibur/ceres/lib.sh
@@ -1,7 +1,16 @@
+#koitetaanpa jyrätä tämä aiempi määrittely (ntp kusi)
+#TODO:ntp:n siirtäminen distro-spesifisiin fktioihin?
+PART175_LIST="avahi bluetooth cups exim4 nfs network mdadm"
+
 function pre_part3() {
 	dqb "xc.pp3( ${1} , ${2} )"
 	csleep 1
-	#TODO
+
+	#VAIH
+	sudo dpkg -i ~/Desktop/minimize/daedalus/libip*.deb
+	sudo dpkg -i ~/Desktop/minimize/daedalus/daedalus/libxtables12_1.8.9-2_amd64.deb 
+	sudo dpkg -i ~/Desktop/minimize/daedalus/daedalus/iptables_*.deb
+	#kunnes saa export2 toimimaan 
 }
 
 function pr4() {
@@ -16,7 +25,9 @@ function udp6() {
 	#TODO
 }
 
+#HUOM.27525:debug tuntui määräävän, ajetaanko esim. part2_5 , toivottavasti ei toistu
 function part2_pre() {
+	debug=1
 	dqb "xc.PP2"
 	#TODO
 }
