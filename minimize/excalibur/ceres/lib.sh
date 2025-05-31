35a58ef15bdbee71407533cab44962bf922870ca
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-06-01 01:04:55 +0300

    Add files via upload

--- minimize/excalibur/ceres/lib.sh
+++ minimize/excalibur/ceres/lib.sh
@@ -65,11 +65,16 @@ function cp5() {
 }
 
 function pr4() {
+	debug=1
 	dqb "xc.pr4( ${1} , ${2} )"
 	csleep 1
 
 	cp5 ${1}
-	#VAIH
+	csleep 1
+	dqb "XC.PR4.PART2"
+
+	${sdi} ${1}/isc-dhcp-*.deb
+	[ $? -eq 0 ] && ${NKVD} ${1}/isc-dhcp-*.deb
 
 	dqb "...done"
 	csleep 1
@@ -85,10 +90,23 @@ function udp6() {
 	csleep 1
 }
 
+#jotain tämmöistä tähänn fktioon olisi tarkoitus tulla jatkossa
+function tpc7() {
+	dqb "UNDER CONSTRUCTION"
+	#${shary} linux-modules-6.12.27-amd64 #31525 uutena
+
+	#nopeasti lähimpiä vastineita:
+	#https://packages.debian.org/trixie/linux-image-6.12.27-amd64 miten tämä?
+	#https://debian.ethz.ch/debian/pool/main/l/linux-signed-amd64/linux-image-cloud-amd64_6.12.27-1_amd64.deb
+	#wget/curl jos ei muuten
+
+	${shary} nftables #excalibur-spesifisiä?
+	${shary} isc-dhcp-client isc-dhcp-common
+}
+
 function t2p() {
 	debug=1
-
-	dqb "XC.T2P (VAIH)"
+	dqb "XC.T2P"
 	
 	#31525 lisäyksiä (util-linux ei uskalla poistaa, miten nuo muut?)
 	#${sharpy} libsmartcols1 libuuid1 liblastlog2-2
