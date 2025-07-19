3ae76e42812eb7ad875d30316927917f1194f4aa
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-07-20 00:23:05 +0300

    recent changes

--- minimize/export2.sh
+++ minimize/export2.sh
@@ -2,7 +2,7 @@
 debug=0 #1
 tgtfile=""
 distro=$(cat /etc/devuan_version | cut -d '/' -f 1) #HUOM.28525:cut pois jatkossa?
-PREFIX=~/Desktop/minimize #käyttöön+konftdstoon jos mahd #tai dirname? #TODO
+#PREFIX=~/Desktop/minimize #käyttöön+konftdstoon jos mahd #tai dirname? #TODO
 mode=-2
 
 #HUOM.8725.1:joskohan wpa_supplicant.conf kanssa asiat kunnossa
@@ -24,11 +24,11 @@ function parse_opts_1() {
 			if [ ${mode} -eq -2 ] ; then
 				mode=${1}
 			else
-				if [ -d ${PREFIX}/${1} ] ; then #TODO
-					distro=${1}
-				else
-					tgtfile=${1}
-				fi
+				#if [ -d ${PREFIX}/${1} ] ; then #TODO
+				#	distro=${1}
+				#else
+				#	tgtfile=${1}
+				#fi
 			fi
 		;;
 	esac
@@ -38,125 +38,125 @@ function parse_opts_2() {
 	dqb "parseopts_2 ${1} ${2}"
 }
 
-if [ -x ${PREFIX}/common_lib.sh ] ; then #TODO
-	. ${PREFIX}/common_lib.sh
-#else
-#	#HUOM.23525:oleellisempaa että import2 toimii tarvittaessa ilman common_lib
-#	#"lelukirjasto" saattaa toimia sen vErrAn että "$0 4 ..." onnistuu	
-#	
-#	srat="sudo /bin/tar"
-#	som="sudo /bin/mount"
-#	uom="sudo /bin/umount"
-#	scm="sudo /bin/chmod"
-#	sco="sudo /bin/chown"
-#	odio=$(which sudo)
-#
-#	#jos näillä lähtisi aiNAKin case q toimimaan
-#	n=$(whoami)
-#	sah6=$(${odio} which sha512sum)
-#	smr="${odio} which rm"
-#	smr="${odio} ${smr}"
-#
-#	function check_binaries() {
-#		dqb "exp2.ch3ck_b1nar135( ${1} )"
-#	}
-#
-#	function check_binaries2() {
-#		dqb "exp2.ch3ck_b1nar135_2( ${1} )"
+#if [ -x ${PREFIX}/common_lib.sh ] ; then #TODO
+#	. ${PREFIX}/common_lib.sh
+##else
+##	#HUOM.23525:oleellisempaa että import2 toimii tarvittaessa ilman common_lib
+##	#"lelukirjasto" saattaa toimia sen vErrAn että "$0 4 ..." onnistuu	
+##	
+##	srat="sudo /bin/tar"
+##	som="sudo /bin/mount"
+##	uom="sudo /bin/umount"
+##	scm="sudo /bin/chmod"
+##	sco="sudo /bin/chown"
+##	odio=$(which sudo)
+##
+##	#jos näillä lähtisi aiNAKin case q toimimaan
+##	n=$(whoami)
+##	sah6=$(${odio} which sha512sum)
+##	smr="${odio} which rm"
+##	smr="${odio} ${smr}"
+##
+##	function check_binaries() {
+##		dqb "exp2.ch3ck_b1nar135( ${1} )"
 ##	}
-#
-#	function fix_sudo() {
-#		dqb "exp32.fix.sudo"
-#	}
-#
-#	function enforce_access() {
-#		dqb "exp32.enf_acc()"
-#	}
-#
-#	function part3() {
-#		dqb "exp32.part3()"
-#	}
-#
-#	function part1_5() {
-#		dqb "exp32.p15()"
-#	}
-#
-#	function message() {
-#		dqb "exp32.message()"
-#	}
-#
-#	function jules() {
-#		dqb "exp32.jules()"
-#	}
-#
-#	#HUOM.23525:josko tässä kohtaa pakotus riittäisi
-#	function other_horrors() {
-#		dqb "AZATHOTH AND OTHER HORRORS"
-#		#${spc} /etc/default/rules.* /etc/iptables #tartteeko tuota enää 27525?
-#
-#		${scm} 0400 /etc/iptables/*
-#		${scm} 0550 /etc/iptables
-#		${sco} -R root:root /etc/iptables
-#		${scm} 0400 /etc/default/rules*
-#		${scm} 0555 /etc/default
-#		${sco} -R root:root /etc/default
-#	}
-#
-#	function ppp3() {
-#		dqb "exp32.ppp3()"
-#	}
-#
-#	prevopt=""
-#
-#	for opt in $@ ; do
-#		parse_opts_1 ${opt}
-#		parse_opts_2 ${prevopt} ${opt}
-#		prevopt=${opt}
-#	done
-#
-#	dqb "FALLBACK"
-#	dqb "chmod may be a good idea now"
-fi
+##
+##	function check_binaries2() {
+##		dqb "exp2.ch3ck_b1nar135_2( ${1} )"
+###	}
+##
+##	function fix_sudo() {
+##		dqb "exp32.fix.sudo"
+##	}
+##
+##	function enforce_access() {
+##		dqb "exp32.enf_acc()"
+##	}
+##
+##	function part3() {
+##		dqb "exp32.part3()"
+##	}
+##
+##	function part1_5() {
+##		dqb "exp32.p15()"
+##	}
+##
+##	function message() {
+##		dqb "exp32.message()"
+##	}
+##
+##	function jules() {
+##		dqb "exp32.jules()"
+##	}
+##
+##	#HUOM.23525:josko tässä kohtaa pakotus riittäisi
+##	function other_horrors() {
+##		dqb "AZATHOTH AND OTHER HORRORS"
+##		#${spc} /etc/default/rules.* /etc/iptables #tartteeko tuota enää 27525?
+##
+##		${scm} 0400 /etc/iptables/*
+##		${scm} 0550 /etc/iptables
+##		${sco} -R root:root /etc/iptables
+##		${scm} 0400 /etc/default/rules*
+##		${scm} 0555 /etc/default
+##		${sco} -R root:root /etc/default
+##	}
+##
+##	function ppp3() {
+##		dqb "exp32.ppp3()"
+##	}
+##
+##	prevopt=""
+##
+##	for opt in $@ ; do
+##		parse_opts_1 ${opt}
+##		parse_opts_2 ${prevopt} ${opt}
+##		prevopt=${opt}
+##	done
+##
+##	dqb "FALLBACK"
+##	dqb "chmod may be a good idea now"
+#fi
 
 [ -z ${distro} ] && exit 6
-d=${PREFIX}/${distro} #TODO
+#d=${PREFIX}/${distro} #TODO
 
 dqb "mode= ${mode}"
 dqb "distro=${distro}"
 dqb "file=${tgtfile}"
 csleep 2
 
-if [ -s ${d}/conf ] ; then
-	. ${d}/conf
-else #joutuukohan else-haaran muuttamaan jatkossa?
-	echo "CONF MISSING"
-	exit 55
-fi
-
-if [ -d ${d} ] && [ -x ${d}/lib.sh ] ; then
-	. ${d}/lib.sh
-else
-	echo "NOT (L1B AVA1LABL3 AND 3X3CUT4BL3)"
-
-	function pr4() {
-		dqb "exp2.pr4 ${1} " 
-	}
-
-	function udp6() {
-		dqb "exp32.UPD6()"
-	}
-
-	#onko tässä sktiptissä oleellista välittää $d part3:lle asti c_b välityksellä?
-	check_binaries ${d}
-	check_binaries2
-fi
+#if [ -s ${d}/conf ] ; then
+#	. ${d}/conf
+#else #joutuukohan else-haaran muuttamaan jatkossa?
+#	echo "CONF MISSING"
+#	exit 55
+#fi
+#
+#if [ -d ${d} ] && [ -x ${d}/lib.sh ] ; then
+#	. ${d}/lib.sh
+#else
+#	echo "NOT (L1B AVA1LABL3 AND 3X3CUT4BL3)"
+#
+#	function pr4() {
+#		dqb "exp2.pr4 ${1} " 
+#	}
+#
+#	function udp6() {
+#		dqb "exp32.UPD6()"
+#	}
+#
+#	#onko tässä sktiptissä oleellista välittää $d part3:lle asti c_b välityksellä?
+#	check_binaries ${d}
+#	check_binaries2
+#fi
 
 function usage() {
 	echo "$0 0 <tgtfile> [distro] [-v]: makes the main package (new way)"
 	echo "$0 4 <tgtfile> [distro] [-v]: makes lighter main package (just scripts and config)"
 	echo "$0 1 <tgtfile> [distro] [-v]: makes upgrade_pkg"
 	echo "$0 e <tgtfile> [distro] [-v]: archives the Essential .deb packages"
-	echo "$0 f <tgtfile> [distro] [-v]: archives .deb Files under \${PREFIX}/\${distro}" #TODO
+	#echo "$0 f <tgtfile> [distro] [-v]: archives .deb Files under \${PREFIX}/\${distro}" #TODO
 	echo "$0 p <> [] [] pulls Profs.sh from somewhere"
 	echo "$0 q <> [] [] archives firefox settings"
 	echo "$0 t ... option for ipTables"			
@@ -164,8 +164,8 @@ function usage() {
 }
 
 dqb "tar = ${srat} "
-${scm} 0555 ${PREFIX}/changedns.sh #TODO
-${sco} root:root ${PREFIX}/changedns.sh #TODO
+#${scm} 0555 ${PREFIX}/changedns.sh #TODO
+#${sco} root:root ${PREFIX}/changedns.sh #TODO
 tig=$(${odio} which git)
 mkt=$(${odio} which mktemp)
 
@@ -391,7 +391,7 @@ function rmt() { #HUOM.16725:toiminee muuten mutta param tark vähn pykii
 
 #HUOM.25525:tapaus excalibur/ceres teettäisi lisähommia, tuskin menee qten alla
 tcdd=$(cat /etc/devuan_version)
-t2=$(echo ${d} | cut -d '/' -f 6 | tr -d -c a-zA-Z/) #tai suoraan $distro?
+#t2=$(echo ${d} | cut -d '/' -f 6 | tr -d -c a-zA-Z/) #tai suoraan $distro?
 	
 if [ ${tcdd} != ${t2} ] ; then
 	dqb "XXX"
@@ -805,14 +805,14 @@ function tp5() { #8725 toiminee
 dqb "mode= ${mode}"
 dqb "tar= ${srat}"
 csleep 1
-pre1 ${d}
+#pre1 ${d}
 
 #18725:skriptin case:t 0/4/e kai toimibat, muiden testaus myös
 #HUOM.20525:pitäisi kai mode:n kanssa suosia numeerisia arvoja koska urputukset
 case ${mode} in
 	0|4) #erikseen vielä case missä tp3 skipataan?
-		pre1 ${d}
-		pre2 ${d}
+		#pre1 ${d}
+		#pre2 ${d}
 
 		${odio} touch ./rnd
 		${sco} ${n}:${n} ./rnd
@@ -820,47 +820,47 @@ case ${mode} in
 		dd if=/dev/random bs=6 count=1 > ./rnd
 
 		${srat} -cvf ${tgtfile} ./rnd
-		tp3 ${tgtfile} ${distro} #voisiko käyttää $d?
+		#tp3 ${tgtfile} ${distro} #voisiko käyttää $d?
 
-		[ -f ${d}/e.tar ] && ${NKVD} ${d}/e.tar
-		${srat} -cvf ${d}/e.tar ./rnd #tarvitseeko random-kuraa 2 kertaan?
-		[ ${mode} -eq 0 ] && tp4 ${d}/e.tar ${d}
+		#[ -f ${d}/e.tar ] && ${NKVD} ${d}/e.tar
+		#${srat} -cvf ${d}/e.tar ./rnd #tarvitseeko random-kuraa 2 kertaan?
+		#[ ${mode} -eq 0 ] && tp4 ${d}/e.tar ${d}
 		${sifd} ${iface}
 
 		#HUOM.22525: pitäisi kai reagoida siihen että e.tar enimmäkseen tyhjä?
-		tp1 ${tgtfile} ${d}
-		pre1 ${d}
+		#tp1 ${tgtfile} ${d}
+		#pre1 ${d}
 		tp2 ${tgtfile}
 	;;
 	1|u|upgrade)
-		pre2 ${d}
-		tpu ${tgtfile} ${d}
+		#pre2 ${d}
+		#tpu ${tgtfile} ${d}
 	;;
 	p)
 		#HUOM.240325:tämä+seur case toimivat, niissä on vain semmoinen juttu(kts. S.Lopakka:Marras)
-		pre2 ${d}
-		tp5 ${tgtfile} ${PREFIX} #PREFIX vähemmäLLe jatkossa? #TODO
+		#pre2 ${d}
+		#tp5 ${tgtfile} ${PREFIX} #PREFIX vähemmäLLe jatkossa? #TODO
 	;;
 	e)
-		pre2 ${d}
-		tp4 ${tgtfile} ${d}
+		#pre2 ${d}
+		#tp4 ${tgtfile} ${d}
 	;;
 	f)
-		rmt ${tgtfile} ${d}
+		#rmt ${tgtfile} ${d} #HUOM. ei kai oleellista päästä ajelemaan tätä skriptiä chroootin sisällä, generic ja import2 olennaisempia
 	;;
 	q)
 		[ z"${tgtfile}" == "z" ] && exit 99
 		${sifd} ${iface}
 
-		tpq ${PREFIX} #TODO
-		${srat} -cf ${tgtfile} ${PREFIX}/config.tar.bz2 ${PREFIX}/fediverse.tar #TODO
+		#tpq ${PREFIX} #TODO
+		#${srat} -cf ${tgtfile} ${PREFIX}/config.tar.bz2 ${PREFIX}/fediverse.tar #TODO
 	;;
 	t)
-		pre2 ${d}
-		${NKVD} ${d}/*.deb
-		tlb ${d}
-		${svm} ${pkgdir}/*.deb ${d}
-		rmt ${tgtfile} ${d}
+		#pre2 ${d}
+		#${NKVD} ${d}/*.deb
+		#tlb ${d}
+		#${svm} ${pkgdir}/*.deb ${d}
+		#rmt ${tgtfile} ${d}
 	;;
 	-h)
 		usage
