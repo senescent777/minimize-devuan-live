3c5f5e285207733d5b961d1658c3f3e91b8cdb45
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-07-29 21:40:52 +0300

    comment

--- minimize/generic_pt2.sh
+++ minimize/generic_pt2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#TODO:vielä juttuja pakettien poisteluihin liittyen? (daed/lib.sh)
+#TODO:vielä juttuja pakettien poisteluihin liittyen? (daed/lib.sh) vielä tarpeen 29725?
 distro=$(cat /etc/devuan_version) #tämä tarvitaan toistaiseksi
 d0=$(pwd)
-echo "d0=${d0}"
+#echo "d0=${d0}"
 [ z"${distro}" == "z" ] && exit 6
 debug=0
 d=${d0}/${distro}
