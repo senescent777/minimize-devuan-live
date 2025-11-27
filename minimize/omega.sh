78cbca920eb62020f49618c749909e5879e87f82
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-11-27 10:37:44 +0000

    c

--- minimize/omega.sh
+++ minimize/omega.sh
@@ -14,8 +14,8 @@ if [ $# -gt 0 ] ; then
 	mode=${1}
 fi
 
-#251125:edelleen päivityspak ajamisesta seuraa "login command failed", onkohan kyse jostain muustaq hmistojen käyttöoik?
-#... jos ei muuta keksi ni slim pois kiekolta? tai pikemminkin minimal livecd pohjaksi?
+#251125:edelleen päivityspak ajamisesta seuraa "login command failed", kyse lienee muustaq hmistojen käyttöoik
+#... jos ei muuta keksi ni slim pois kiekolta/esim lxdm tilalle? tai pikemminkin minimal livecd pohjaksi?
 
 [ ${mode} -gt 1 ] && ${odio} usermod -G devuan,cdrom,floppy,audio,dip,video,plugdev,netdev devuan 
 [ ${mode} -gt 2 ] && ${smr} /etc/sudoers.d/live
