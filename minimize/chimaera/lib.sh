59697e2bacfbb525f046ee672a93b6c36a22f505
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-02-25 20:32:08 +0000

    more of those tests w/ chimaera

--- minimize/chimaera/lib.sh
+++ minimize/chimaera/lib.sh
@@ -1,86 +1,35 @@
 #=================================================PART 0=====================================
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
 
-#milloin viimeksi testattu? lokakuussa? 160126 myös testailuja menossa
-function udp6() {
-	dqb "ch1m.lib.UPDP-6"
-	csleep 1
-
-	dqb "do_Something() ?"
-
-	dqb "D0NE"
-	csleep 1
-}
-
-#function t2p() { #130226:korvautuu kohta g_pt2 uudella fktiolla tai siis
+#250226:tämän fktion voisi vähitellen hukata? cg_udp6() tilalle jo?
+#function udp6() {
+#	dqb "ch1m.lib.UPDP-6"
+#	csleep 1
+#
+#	dqb "do_Something() ?"
+#
+#	dqb "D0NE"
+#	csleep 1
+#}
+#
+#function t2p() { 
 #	csleep 1
 #
 #	dqb "chim.t2p()"
 #	csleep 1
 #
-#	${sharpy} atril* #daedaluksess poistui aiemmin
-#	${lftr}
-#	${sharpy} bc bluez #daed
-#	t2p_filler
 #
-#	#dconf,debconf off limits
-#	#HUOM. miten gdisk, löytyykö?
-#	${sharpy} ghostscript #ei daed
-#
-#	#gnome* , gpg* off limits , gnupg* ei löydy
-#	${sharpy} gparted* #daud ei löydy
-#	t2p_filler
 #	
-#	#gsettings-desktop-schemas off limits
-#	${sharpy} grub* #HUOM.28525:syystä excalibur prujattu tähän
-#	${sharpy} gsasl-common #eilöydy d
-#	${sharpy} gsfonts* #eilöydy
-#	t2p_filler
 #
-#	${sharpy} gvfs* #eilöydy
-#	t2p_filler
 #	
-#	#löytyykö chimaerasta iucode-tool?
-#	#lib-jutut jos antaisi enimmäkseen olla rauhassa
-#	#linux* , live* off limits
-#	#lp-solve ei löytynyt?	
-#	#${sharpy} mariadb-common #ei löytynyt d
+#	
 #
-#	${sharpy} mdadm # ei d
-#	${sharpy} mobile* #ei d
-#	t2p_filler
 #
-#	${sharpy} mysql-common #ei d?
-#	t2p_filler
 #
-#	#ntfs-3g?
-#	${sharpy} ntp* #HUOM.28525:syystä excalibur prujattu tähän	
-#	${sharpy} notification-daemon #ei löydy d
-#	t2p_filler
 #
-#	${sharpy} orca packagekit* #ei löydy d
-#	#{sharpy} p7zip ?
-#	t2p_filler
+#	
 #
-#	#löytyykö plocate?
-#	${sharpy} pigz #ei löydy d
-#	${sharpy} po* #ei löydy d
-#	t2p_filler
 #
-#	#procps off limits
-#	#löytyykö psmisc? entä rsync?
-#	${sharpy} rpcbind #miten rsync?
-#	${sharpy} sane-utils #ei löydy d
-#	t2p_filler
-#
-#	${sharpy} squash* #löytyykö d?
-#	t2p_filler
-#
-#	#traceroute?
-#	${sharpy} telnet #tartteeko erikseen? olisi se inetutils-telnet myös
-#
-#	#upower? w3m? wget? xarchiver?
-#	#xfce*,xorg* off limits
 #
 #	dqb "D0N3"
 #	csleep 1
