cc66a34a87184071c140fea75be4f8ee4bba2b1f
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-02-25 18:05:16 +0000

    recent/250226/2

--- minimize/daedalus/lib.sh
+++ minimize/daedalus/lib.sh
@@ -32,77 +32,17 @@ function udp6() { #on käytössä
 #251225:poistetaanko täsäs kohtaa liikaa?
 #130126:sqrot-tstissä psmisc ei poistunut
 
-#VAIH:p2g() käyttöön
+#VAIH:p2g() käyttöön (VÄHITELLEN VARMISTA ETTÄ TÄSÄS LISTATUT POISTUVAT) vissiin jouta ajo poistaa 25226
 #function t2p() { 
 #	dqb "DAUD.T2P"
 #	csleep 1
 #
-#	${sharpy} arch-test
-#	${sharpy} grub* #oliko tämän poiston kanssa jokin juttu?
-#	${sharpy} gsettings* #tähän asti ok?
-#	t2p_filler
 #
-#	${sharpy} iucode-tool
-#	t2p_filler
-#
-#	dqb "TODO:ntpsec hyötykäyttö"
-#	csleep 2
-#
-#	${sharpy} ntp* #121125:tässä base-passwd- ja init-valitusta. vielä 231125?
-#	${sharpy} ntfs-3g
-#	t2p_filler
-#
-#	${sharpy} p7zip
-#	t2p_filler
-#
-#	${sharpy} psmisc #tästä tuli initramfs-valitus
-#	t2p_filler
-#
-#	${sharpy} rsync squashfs-tools #eiole?
-#	t2p_filler
-#
-#	${sharpy} traceroute #eiole?
-#	t2p_filler
-#
-#	${sharpy} upower #tästä tuli initramfs-valitus
-#	t2p_filler
-#
-#	${sharpy} w3m wget #eiole?
-#	t2p_filler
-#
-#	dqb "D0N3"
-#	csleep 1
-#
-#	echo "DERTHAGO 3ST KALENDAM"
-#	csleep 6
-#
-#	${sharpy} xorriso* #eiole?
-#	${asy} #initramfs...
-#	csleep 2
-#
-#	${sharpy} xorriso*
-#	${asy}
-#	csleep 2
-#
-#	${sharpy} xz* #initramfs
-#	${asy}
-#	csleep 2
-#
-#	${sharpy} xfburn  #eiole
-#	${asy}
-#	sleep 2
-#
-#	${sharpy} xarchiver #¤eiole+initranmfs
-#	${asy}
-#	sleep 2
 #
 #	#${sharpy} libdav* #121125:tämä näyttää poistavan paljon tapauksessa daed, ehkä jopa liikaa
 #	#ÄLÄ SIIS WTUN PÖSILÖ POISTA libdav-PAKETTEJA ELLEI KIINNOSTA SELVITELLÄ "ERROR: ld.so: object 'libgtk3-nocsd.so.0' from LD_PRELOAD cannot be preloaded (cannot open shared object file): ignored."-NALKUTUSTA
 #	#${asy} #varm. vuoksi
 #	#sleep 2
-#
-#	${scm} a-wx ${0}
-#	csleep 2
 #}
 
 #josko kuitenkin ntp takaisin part2_5_listaan?
