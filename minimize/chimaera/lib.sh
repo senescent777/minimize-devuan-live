722ba212fb99634f3b056ec22ab7ad462b553c0a
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-02-27 21:48:39 +0000

    rfecent/c

--- minimize/chimaera/lib.sh
+++ minimize/chimaera/lib.sh
@@ -1,39 +1,6 @@
 #=================================================PART 0=====================================
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
-
-#250226:tämän fktion voisi vähitellen hukata? cg_udp6() tilalle jo?
-#function udp6() {
-#	dqb "ch1m.lib.UPDP-6"
-#	csleep 1
 #
-#	dqb "do_Something() ?"
-#
-#	dqb "D0NE"
-#	csleep 1
-#}
-#
-#function t2p() { 
-#	csleep 1
-#
-#	dqb "chim.t2p()"
-#	csleep 1
-#
-#
-#	
-#
-#	
-#	
-#
-#
-#
-#
-#	
-#
-#
-#
-#	dqb "D0N3"
-#	csleep 1
-#} 
 
 function pre_part2() {
 	dqb "ch1m.pre_part2()"
