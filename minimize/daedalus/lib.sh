fe625fa2522063278d579b5a1f2b3ce85f70f872
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-02-28 18:47:59 +0000

    recent/d

--- minimize/daedalus/lib.sh
+++ minimize/daedalus/lib.sh
@@ -3,8 +3,6 @@
 #https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
 
-#common_lib_tool-tyylillä jatkossa tämänkin fktion hommat? kts g_pt2
-#251225:poistetaanko täsäs kohtaa liikaa?
 #130126:sqrot-tstissä psmisc ei poistunut
 
 #function t2p() { 
@@ -34,13 +32,13 @@ function pre_part2() { #käytössä
 }
 
 #https://pkginfo.devuan.org/cgi-bin/package-query.html?c=package&q=netfilter-persistent=1.0.20
-#https://pkgs.org/download/linux-image-6.12.27-amd64 ... joskohan ethz kautta
+
 #... tarkistus tosin uusiksi, josko sinne tcdd-blokkiin(?) ylemmäs?
 
 function tpc7() { #e22.sh kutsuu tätä nykyään
 	dqb "d.prc7 UNDER CONSTRUCTION"
 }
 
-check_binaries ${d}
+#check_binaries ${d}
 lftr="echo # \${smr} -rf  / run / live / medium / live / initrd.img\* " 	
-check_binaries2
+#check_binaries2
