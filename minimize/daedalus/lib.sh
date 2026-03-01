db803cef0682d76b037610312ceb555244d763c9
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-03-01 19:03:11 +0000

    random/d

--- minimize/daedalus/lib.sh
+++ minimize/daedalus/lib.sh
@@ -2,8 +2,7 @@
 
 #https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
-
-#130126:sqrot-tstissä psmisc ei poistunut
+#130126:sqrot-tstissä psmisc ei poistunut (miten nkyään?)
 
 #function t2p() { 
 #	dqb "DAUD.T2P"
@@ -32,13 +31,10 @@ function pre_part2() { #käytössä
 }
 
 #https://pkginfo.devuan.org/cgi-bin/package-query.html?c=package&q=netfilter-persistent=1.0.20
-
 #... tarkistus tosin uusiksi, josko sinne tcdd-blokkiin(?) ylemmäs?
 
 function tpc7() { #e22.sh kutsuu tätä nykyään
 	dqb "d.prc7 UNDER CONSTRUCTION"
 }
 
-#check_binaries ${d}
 lftr="echo # \${smr} -rf  / run / live / medium / live / initrd.img\* " 	
-#check_binaries2
