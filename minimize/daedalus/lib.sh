9693e7a1751e76e6ca0c983ff086fd6f831042bd
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-03-08 23:52:34 +0000

    avoiding merging conflicts pt 3

--- minimize/daedalus/lib.sh
+++ minimize/daedalus/lib.sh
@@ -2,21 +2,9 @@
 
 #https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
+#130126:sqrot-tstissä psmisc ei poistunut (miten nkyään?)
 
-#common_lib_tool-tyylillä jatkossa tämänkin fktion hommat? kts g_pt2
-#251225:poistetaanko täsäs kohtaa liikaa?
-#130126:sqrot-tstissä psmisc ei poistunut
-
-#function t2p() { 
-#	dqb "DAUD.T2P"
-#	csleep 1
-#	#${sharpy} libdav* #121125:tämä näyttää poistavan paljon tapauksessa daed, ehkä jopa liikaa
-#	#ÄLÄ SIIS WTUN PÖSILÖ POISTA libdav-PAKETTEJA ELLEI KIINNOSTA SELVITELLÄ "ERROR: ld.so: object 'libgtk3-nocsd.so.0' from LD_PRELOAD cannot be preloaded (cannot open shared object file): ignored."-NALKUTUSTA
-#	#${asy} #varm. vuoksi
-#	#sleep 2
-#}
-
-#josko kuitenkin ntp takaisin-> part175?
+#020326:annetaanpa toistaiseksi olla PART175_LIST kuten on ja p_p2 myös
 function pre_part2() { #käytössä
 	dqb "daud.pre_part2()"
 	csleep 2
@@ -34,13 +22,10 @@ function pre_part2() { #käytössä
 }
 
 #https://pkginfo.devuan.org/cgi-bin/package-query.html?c=package&q=netfilter-persistent=1.0.20
-#https://pkgs.org/download/linux-image-6.12.27-amd64 ... joskohan ethz kautta
 #... tarkistus tosin uusiksi, josko sinne tcdd-blokkiin(?) ylemmäs?
 
 function tpc7() { #e22.sh kutsuu tätä nykyään
 	dqb "d.prc7 UNDER CONSTRUCTION"
 }
 
-check_binaries ${d}
 lftr="echo # \${smr} -rf  / run / live / medium / live / initrd.img\* " 	
-check_binaries2
