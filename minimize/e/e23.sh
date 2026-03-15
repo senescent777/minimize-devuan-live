b519c9df38a889f8fe03239353e1d5cc356aa047
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-03-15 20:53:12 +0000

    some funcs temporarily disabled

--- minimize/e/e23.sh
+++ minimize/e/e23.sh
@@ -116,49 +116,49 @@
 #		
 #	csleep 2
 #}
-
-#äksän kanssa "+scm +usermod -seatd" se toimiva jekku?
-
-function e23_upgp() {
-	dqb " e23_upgp() "
-
-	dqb "pars_ok"
-	${fib}
-	csleep 1
-
-	#LOPPUU SE PURPATUS PRKL
-	${shary} cpp-12 gcc-12-base libstdc++6 
-	${shary} libgcc-s1 libc6 libgomp1 
-
-	#helpompi vain ajaa e23_dm() ennen upgp()
-	${sag} --no-install-recommends upgrade -u
-	echo $?
-
-	#HUOM.081225:pitäisiköhän keskeyttää tässä jos upgrade qsee?
-	csleep 1
-
-	dqb " e23_upgp() done"
-	csleep 1
-}
-
-function e23_upgp2() {
-	[ -z "${1}" ] && exit 1 
-	[ -z "${2}" ] && exit 11
-
-	case ${2} in
-		wlan0)
-			csleep 1
-		;;
-		*)
-			${NKVD} ${1}/wpa*
-			#HUOM.25725:pitäisi kai poistaa wpa-paketit tässä, aptilla myös?
-			#... vai lähtisikö vain siitä että g_pt2 ajettu ja täts it
-		;;
-	esac
-
-	dqb " e23_upgp2() done"
-	csleep 1
-}
+#
+##äksän kanssa "+scm +usermod -seatd" se toimiva jekku?
+#
+#function e23_upgp() {
+#	dqb " e23_upgp() "
+#
+#	dqb "pars_ok"
+#	${fib}
+#	csleep 1
+#
+#	#LOPPUU SE PURPATUS PRKL
+#	${shary} cpp-12 gcc-12-base libstdc++6 
+#	${shary} libgcc-s1 libc6 libgomp1 
+#
+#	#helpompi vain ajaa e23_dm() ennen upgp()
+#	${sag} --no-install-recommends upgrade -u
+#	echo $?
+#
+#	#HUOM.081225:pitäisiköhän keskeyttää tässä jos upgrade qsee?
+#	csleep 1
+#
+#	dqb " e23_upgp() done"
+#	csleep 1
+#}
+#
+#function e23_upgp2() {
+#	[ -z "${1}" ] && exit 1 
+#	[ -z "${2}" ] && exit 11
+#
+#	case "${2}" in
+#		wlan0)
+#			csleep 1
+#		;;
+#		*)
+#			${NKVD} ${1}/wpa*
+#			#HUOM.25725:pitäisi kai poistaa wpa-paketit tässä, aptilla myös?
+#			#... vai lähtisikö vain siitä että g_pt2 ajettu ja täts it
+#		;;
+#	esac
+#
+#	dqb " e23_upgp2() done"
+#	csleep 1
+#}
 #
 ##080326:ehkä toimii
 #function e23_ghi() {
@@ -173,190 +173,190 @@ function e23_upgp2() {
 #	echo "$0 f ${1} ${3}"
 #}
 #
-#140326:toimi
-function e23_qrs() {
+##140326:toimi
+#function e23_qrs() {
+##
+##
+##	[ -z "${1}" ] && exit 77
+##	[ -s ${1} ] || exit 66
+##	[ -r ${1} ] || exit 55
+##
+##	[ -z "${2}" ] && exit 11
+##	[ -d ${2} ] || exit 22
+##
+##	[ -z "${3}" ] && exit 44
+##	#[ -f ${3} ] || exit 33
+##
+##	[ -z "${4}" ] && exit 43
+##	#[ -f ${4} ] || exit 34
+##
+##	[ -z "${5}" ] && exit 43
+##	#[ -f ${5} ] || exit 34
 #
+#	dqb "pars.0k"
+#	csleep 1
 #
-#	[ -z "${1}" ] && exit 77
-#	[ -s ${1} ] || exit 66
-#	[ -r ${1} ] || exit 55
+#	e22_config1 ~ ${3}
+#	${srat} -rvf ${1} ~/${3}
+#	csleep 2
 #
-#	[ -z "${2}" ] && exit 11
-#	[ -d ${2} ] || exit 22
+#	#tuleeko mukaan vai ei?
+#	tar -tf ${1} | grep ${3} | wc -l
+#	csleep 3
 #
-#	[ -z "${3}" ] && exit 44
-#	#[ -f ${3} ] || exit 33
+#	dqb "BEFORE NVDk"
+#	[ -f ${4} ] && ${NKVD} ~/${4}
+#	csleep 1
 #
-#	[ -z "${4}" ] && exit 43
-#	#[ -f ${4} ] || exit 34
+#	e22_settings ${2} ${4} ${5}
+#	#btw. mikä olikaan syy että q on tässä ekassa switch-case:ssa? pl siis että turha apt-renkkaus
+#	exit 99
 #
-#	[ -z "${5}" ] && exit 43
-#	#[ -f ${5} ] || exit 34
-
-	dqb "pars.0k"
-	csleep 1
-
-	e22_config1 ~ ${3}
-	${srat} -rvf ${1} ~/${3}
-	csleep 2
-
-	#tuleeko mukaan vai ei?
-	tar -tf ${1} | grep ${3} | wc -l
-	csleep 3
-
-	dqb "BEFORE NVDk"
-	[ -f ${4} ] && ${NKVD} ~/${4}
-	csleep 1
-
-	e22_settings ${2} ${4} ${5}
-	#btw. mikä olikaan syy että q on tässä ekassa switch-case:ssa? pl siis että turha apt-renkkaus
-	exit 99
-
-	#dqb "just BEFORE find ${2} -maxdepth 1 -type f -name ${4} "
-	#csleep 1
-	exit 99
-
-#	#jospa ei hipsuja tähän find:iin
-#	for f in $(find ${2} -maxdepth 1 -type f -name ${4} -or -name ${5} | grep -v pulse) ; do
-#		${srat} -rvf ${1} ${f}
-#	done
-
-	[ ${debug} -eq 1 ] && tar -tf ${1} | grep ${4} | wc -l
-	csleep 3
-
-	#ftr() tässä vai kutsuvassa koodissa?
-	e22_ftr ${1}
-}
-
-#110326 viimeksi testattu, toimi silloin
-function e23_dm() {
-	[ -z "${1}" ] && exit 11
-	csleep 4
-
-	${fib}
-	csleep 2
-	
-	#LOPPUU SE PURPATUS PRKL
-	${shary} cpp-12 gcc-12-base libstdc++6 
-	${shary} libgcc-s1 libc6 libgomp1 
-	csleep 1
-
-	${shary} libice6 libsm6 libx11-6 libxext6 libxmu6 libxt6
-	${shary} menu twm
-	csleep 1
-	
-	#libselinux oikeastaan muualla jo
-	${shary} libcrypt1 libpam0g libselinux1 #jemmaan?
-	${shary} libxau6 libxaw7 libxdmcp6 libxft2 libxinerama1 
-	csleep 1
-	
-	${shary} libxpm4 libxrender1 debconf x11-utils cpp lsb-base x11-xserver-utils procps
-	csleep 1
-
-	${shary} libgtk-3-0 libgtk-3-common
-	csleep 1
-
-	${shary} libxxf86vm1 libxrandr2 libxml2 libxi6 libglib2.0-0 libglib2.0-data libatk1.0-0 libgdk-pixbuf-2.0-0 libgdk-pixbuf2.0-common
-	csleep 1
-
-	${shary} fontconfig libfribidi0 libharfbuzz0b libthai0
-	${shary} libfreetype6 libxcb-shape0 libxcb-damage0 libxcb-present0 libxcb-xfixes0 libxcb1
-	${shary} libxcb-render0 libxcb-shm0
-
-	case ${1} in
-		xdm) #010126:pitäisiköhän tämäkin case testata?
-			${shary} xdm
-		;;
-		wdm)
-			# zlib1g perl:any xserver-xorg | xserver:tarteeko juuri tässä vetää?
-			${shary} libnuma1
-			${shary} libwebp7 libaom3 libdav1d6 libde265-0 libx265-199
-			${shary} libwebpdemux2 libheif1 libaudit-common libcap-ng0 libaudit1
-			csleep 1
-
-			${shary} libdb5.3 libpam-modules-bin libpam-modules libpam-runtime
-			${shary} sysvinit-utils libtinfo6 libpng16-16 libx11-xcb1  
-			csleep 1
-
-			${shary} libxfixes3 libxcursor1
-			${shary} libxkbfile1 libxmuu1 
-			#bsdextrautils | bsdmainutils 
-			${shary} bsdextrautils groff-base libgdbm6 libpipeline1 libseccomp2 man-db
-			csleep 1
-
-			${shary} libexpat1 fontconfig-config libfontconfig1
-			${shary} libfontenc1 libglvnd0 libglx0 libgl1  
-			${shary} x11-common libxtst6 libxv1 libxxf86dga1 
-			csleep 1
-
-			${shary} psmisc x11-apps
-			${shary} libpcre2-8-0 libpango-1.0-0 libpangoft2-1.0-0 libpangoxft-1.0-0
-			csleep 1
-
-			${shary} libgif7 libwraster6 libjpeg62-turbo
-			${shary} imagemagick-6-common libmagickwand-6.q16-6 libtiff6
-			#csleep 1
-
-			${shary} libbz2-1.0 libfftw3-double3  libjbig0 liblcms2-2 liblqr-1-0 libltdl7 liblzma5 libopenjp2-7 libwebpmux3
-			csleep 1
-
-			${shary} libmagickcore-6.q16-6 
-			${shary} libwutil5 wmaker-common libwings3
-			csleep 1
-
-			${shary} libx11-data libmd0 libbsd0
-			${shary} wdm
-		;;
-#		lxdm)
-#			
-#			${shary} libpixman-1-0
-#			csleep 1
-#			
-#			#jos aikoo dbusista eroon ni libcups2 asennus ei hyvä idea
-#			
-#			# (>= 1.28.3),  (>= 1.28.3),(>= 1.28.3),(>= 2:1.4.99.1),  (>= 1:0.4.5), libxcursor1 (>> 1.1.2), libxdamage1 (>= 1:1.1), , libxfixes3,  (>= 2:1.1.4),  (>= 2:1.5.0),  adwaita-icon-theme | gnome-icon-theme, hicolor-icon-theme, shared-mime-info
-#			${shary} libpangocairo-1.0-0   
-#			csleep 1
+#	#dqb "just BEFORE find ${2} -maxdepth 1 -type f -name ${4} "
+#	#csleep 1
+#	exit 99
 #
-#			${shary} libdeflate0 debliblerc4 
-#			csleep 1
+##	#jospa ei hipsuja tähän find:iin
+##	for f in $(find ${2} -maxdepth 1 -type f -name ${4} -or -name ${5} | grep -v pulse) ; do
+##		${srat} -rvf ${1} ${f}
+##	done
 #
-#			#acceptiin ainakin 2-0-common enne 2-0 ja sitten muuta tauhkaa hakien tässä kunnes alkaa riittää
-#			${shary} libcairo2 libgtk2.0-common libgtk2.0-0
-#			csleep 1
+#	[ ${debug} -eq 1 ] && tar -tf ${1} | grep ${4} | wc -l
+#	csleep 3
+#
+#	#ftr() tässä vai kutsuvassa koodissa?
+#	e22_ftr ${1}
+#}
+#
+##110326 viimeksi testattu, toimi silloin
+#function e23_dm() {
+#	[ -z "${1}" ] && exit 11
+#	csleep 4
+#
+#	${fib}
+#	csleep 2
 #	
-#			#gdk ennen gtk?
-#			${shary}   
-#			csleep 1
-#		
-#			${shary} gtk2-engines-pixbuf gtk2-engines 
-#			csleep 1
+#	#LOPPUU SE PURPATUS PRKL
+#	${shary} cpp-12 gcc-12-base libstdc++6 
+#	${shary} libgcc-s1 libc6 libgomp1 
+#	csleep 1
 #
-#			${shary} lxdm 
-#			csleep 1
-#			
-#			#261225:lxde-juttujrn ja lxpolkit:in riippuvuukisien selvutys saattaa osoittautua tarpeelliskeis?
-#			
-#			#polkit-1-auth-agent:
-#			
-#			#${shary} lxsession-data libpolkit-agent-1-0 libpolkit-gobject-1-0 policykit-1 laptop-detect lsb-release
-#			#csleep 1
-#			 
-#			#	 lxlock | xdg-utils, 
+#	${shary} libice6 libsm6 libx11-6 libxext6 libxmu6 libxt6
+#	${shary} menu twm
+#	csleep 1
+#	
+#	#libselinux oikeastaan muualla jo
+#	${shary} libcrypt1 libpam0g libselinux1 #jemmaan?
+#	${shary} libxau6 libxaw7 libxdmcp6 libxft2 libxinerama1 
+#	csleep 1
+#	
+#	${shary} libxpm4 libxrender1 debconf x11-utils cpp lsb-base x11-xserver-utils procps
+#	csleep 1
 #
-#			# lxpolkit | polkit-1-auth-agent,  lxsession-logout
-#			
-#			${shary} lxpolkit lxsession-logout lxsession
-#			csleep 1
+#	${shary} libgtk-3-0 libgtk-3-common
+#	csleep 1
+#
+#	${shary} libxxf86vm1 libxrandr2 libxml2 libxi6 libglib2.0-0 libglib2.0-data libatk1.0-0 libgdk-pixbuf-2.0-0 libgdk-pixbuf2.0-common
+#	csleep 1
+#
+#	${shary} fontconfig libfribidi0 libharfbuzz0b libthai0
+#	${shary} libfreetype6 libxcb-shape0 libxcb-damage0 libxcb-present0 libxcb-xfixes0 libxcb1
+#	${shary} libxcb-render0 libxcb-shm0
+#
+#	case ${1} in
+#		xdm) #010126:pitäisiköhän tämäkin case testata?
+#			${shary} xdm
 #		;;
-		*)
-		;;
-	esac
-	
-	E22_GX="libwww-perl xscreensaver-data init-system-helpers libegl1 xscreensaver"
-	${shary} ${E22_GX}  #libsystemd0
-}
-
+#		wdm)
+#			# zlib1g perl:any xserver-xorg | xserver:tarteeko juuri tässä vetää?
+#			${shary} libnuma1
+#			${shary} libwebp7 libaom3 libdav1d6 libde265-0 libx265-199
+#			${shary} libwebpdemux2 libheif1 libaudit-common libcap-ng0 libaudit1
+#			csleep 1
+#
+#			${shary} libdb5.3 libpam-modules-bin libpam-modules libpam-runtime
+#			${shary} sysvinit-utils libtinfo6 libpng16-16 libx11-xcb1  
+#			csleep 1
+#
+#			${shary} libxfixes3 libxcursor1
+#			${shary} libxkbfile1 libxmuu1 
+#			#bsdextrautils | bsdmainutils 
+#			${shary} bsdextrautils groff-base libgdbm6 libpipeline1 libseccomp2 man-db
+#			csleep 1
+#
+#			${shary} libexpat1 fontconfig-config libfontconfig1
+#			${shary} libfontenc1 libglvnd0 libglx0 libgl1  
+#			${shary} x11-common libxtst6 libxv1 libxxf86dga1 
+#			csleep 1
+#
+#			${shary} psmisc x11-apps
+#			${shary} libpcre2-8-0 libpango-1.0-0 libpangoft2-1.0-0 libpangoxft-1.0-0
+#			csleep 1
+#
+#			${shary} libgif7 libwraster6 libjpeg62-turbo
+#			${shary} imagemagick-6-common libmagickwand-6.q16-6 libtiff6
+#			#csleep 1
+#
+#			${shary} libbz2-1.0 libfftw3-double3  libjbig0 liblcms2-2 liblqr-1-0 libltdl7 liblzma5 libopenjp2-7 libwebpmux3
+#			csleep 1
+#
+#			${shary} libmagickcore-6.q16-6 
+#			${shary} libwutil5 wmaker-common libwings3
+#			csleep 1
+#
+#			${shary} libx11-data libmd0 libbsd0
+#			${shary} wdm
+#		;;
+##		lxdm)
+##			
+##			${shary} libpixman-1-0
+##			csleep 1
+##			
+##			#jos aikoo dbusista eroon ni libcups2 asennus ei hyvä idea
+##			
+##			# (>= 1.28.3),  (>= 1.28.3),(>= 1.28.3),(>= 2:1.4.99.1),  (>= 1:0.4.5), libxcursor1 (>> 1.1.2), libxdamage1 (>= 1:1.1), , libxfixes3,  (>= 2:1.1.4),  (>= 2:1.5.0),  adwaita-icon-theme | gnome-icon-theme, hicolor-icon-theme, shared-mime-info
+##			${shary} libpangocairo-1.0-0   
+##			csleep 1
+##
+##			${shary} libdeflate0 debliblerc4 
+##			csleep 1
+##
+##			#acceptiin ainakin 2-0-common enne 2-0 ja sitten muuta tauhkaa hakien tässä kunnes alkaa riittää
+##			${shary} libcairo2 libgtk2.0-common libgtk2.0-0
+##			csleep 1
+##	
+##			#gdk ennen gtk?
+##			${shary}   
+##			csleep 1
+##		
+##			${shary} gtk2-engines-pixbuf gtk2-engines 
+##			csleep 1
+##
+##			${shary} lxdm 
+##			csleep 1
+##			
+##			#261225:lxde-juttujrn ja lxpolkit:in riippuvuukisien selvutys saattaa osoittautua tarpeelliskeis?
+##			
+##			#polkit-1-auth-agent:
+##			
+##			#${shary} lxsession-data libpolkit-agent-1-0 libpolkit-gobject-1-0 policykit-1 laptop-detect lsb-release
+##			#csleep 1
+##			 
+##			#	 lxlock | xdg-utils, 
+##
+##			# lxpolkit | polkit-1-auth-agent,  lxsession-logout
+##			
+##			${shary} lxpolkit lxsession-logout lxsession
+##			csleep 1
+##		;;
+#		*)
+#		;;
+#	esac
+#	
+#	E22_GX="libwww-perl xscreensaver-data init-system-helpers libegl1 xscreensaver"
+#	${shary} ${E22_GX}  #libsystemd0
+#}
+#
 #150326:teki ainakin kerran jotain toivottua (ehkä joutaa vielä arpoa minne juttuja kopsaillaan) 
 function e23_profs() {
 	dqb "e23_profs) $1 , $2 , $3 ("
