b4cec36d5b1db21ce1d6083c98d02e9f6d20f642
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-03-17 20:06:09 +0000

    more code relocated

--- minimize/opt/bin/aftr.bash
+++ minimize/opt/bin/aftr.bash
@@ -77,3 +77,37 @@ if [ -s /etc/resolv.conf ] ; then
 	done
 fi
 
+#================================
+#ntp-kikkailut erilliseen skriptiin?
+#miten muuten ne ipt/netf-persistent? oliko niissä jotain skriptihakemistoa? pystyisikö käyttämään?
+
+function ptn_dda() {
+	local t
+
+	${ipt} -A b -p udp -m udp -s ${t} --sport 123 -j ACCEPT
+	${ipt} -A e -p udp -m udp -d ${t} --dport 123 -j ACCEPT
+}
+
+function ptn2() {
+	if [ -x /usr/sbin/ntpd ] ; then
+		${ipt} -A INPUT -p udp -m udp --sport 123 -j b 
+		${ipt} -A OUTPUT -p udp -m udp --dport 123 -j e
+
+
+		csleep 1
+		dqb "SHOULD START /etc/init.d/ntpsec AROUND HERE"
+		csleep 1
+	fi
+}
+
+function clouds_case0_2() {
+	/etc/init.d/dnsmasq stop
+	${whack} dnsmasq*
+}
+
+function clouds_case1_2() {
+	echo "dns";sleep 1
+	/etc/init.d/dnsmasq restart
+	pgrep dnsmasq
+
+}
