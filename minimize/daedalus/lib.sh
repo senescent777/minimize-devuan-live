8a89f8f2388f07082d731a77ed3c9fcd2d5aa7e3
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-04-05 15:34:19 +0300

    grecent/d

--- minimize/daedalus/lib.sh
+++ minimize/daedalus/lib.sh
@@ -2,24 +2,15 @@
 
 #https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
-#130126:sqrot-tstissä psmisc ei poistunut (miten nkyään?)
+#130126:sqrot-tstissä psmisc ei poistunut (miten nkyään? jos selvittäisi)
 
 #020326:annetaanpa toistaiseksi olla PART175_LIST kuten on ja p_p2 myös
 #... voisi myös vivuta -> common_lib ?
 function pre_part2() { #käytössä
-	dqb "daud.pre_part2()"
-	csleep 2
+	#dqb "daud.pre_part2()"
+	#csleep 2
+	cg_pp2
 
-	${odio} /etc/init.d/ntpd stop
-	#$sharpy ntp* jo aiempana
-
-	for f in $(find /etc/init.d -type f -name 'ntp*' ) ; do 
-		${odio} ${f} stop
-		csleep 1
-	done
-
-	csleep 2
-	dqb "d0n3"
 }
 
 #tekisikö tuolle josqs jotain vaiei?
