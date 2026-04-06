8b34ef2a7da6e1c7799265c0518a4e24b6eda17a
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-04-06 21:51:51 +0300

    more random mutations/6.4.26/2

--- minimize/daedalus/lib.sh
+++ minimize/daedalus/lib.sh
@@ -1,21 +1,20 @@
 #=================================================PART 0=====================================
-
+#
 #https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
 #130126:sqrot-tstissä psmisc ei poistunut (miten nkyään? jos selvittäisi)
-
+#
 #020326:annetaanpa toistaiseksi olla PART175_LIST kuten on ja p_p2 myös
-#... voisi myös vivuta -> common_lib ?
 function pre_part2() { #käytössä
-	#dqb "daud.pre_part2()"
+	dqb "daud.pre_part2()"
 	#csleep 2
-	cg_pp2
+	#cg_pp2
 
 }
-
+#
 #tekisikö tuolle josqs jotain vaiei?
 function tpc7() {
 	dqb "d.prc7 UNDER CONSTRUCTION"
 }
-
-lftr="${smr} -rf /run/live/medium/live/initrd.img* " 	
+#
+#lftr="${smr} -rf /run/live/medium/live/initrd.img* " 	
