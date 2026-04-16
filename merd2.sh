820069e4397573d931770e4e4019be06a4e11fdb
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-04-16 13:48:10 +0300

    just in case

--- merd2.sh
+++ merd2.sh
@@ -4,7 +4,9 @@ branch=""
 d0=$(pwd)
 echo "d0=${d0}"
 CONF_BASEURL="github.com/senescent777"
+CONF_BASE=minimize
 CONF_PT2=minimize-devuan-live
+CONF_LIB=minimize/common_lib.sh
 distro=$(cat /etc/devuan_version)
 
 function dqb() {
@@ -14,6 +16,9 @@ function dqb() {
 function csleep() {
 	[ ${debug} -eq 1 ] && sleep ${1}
 }
+
+#function parse_opts_1 () {}
+#function parse_opts_2 () {}
 
 if [ $# -gt 0 ] ; then
 	dqb "params_ok"
@@ -38,15 +43,18 @@ fi
 dqb "branch=${branch}"
 tig=$(sudo which git)
 
-if [ -z "${tig}"  ] ; then
+if [ x"${tig}" == "x" ] ; then
 	echo "sudo apt-get install git"
 	exit 7
 fi
 
-dqb "rm -rf  ${CONF_PT2} SOON"
-csleep 3
-[ -d ${CONF_PT2} ] && rm -rf ${CONF_PT2}
-[ $? -gt 0 ] && exit
+if [ -d ./${CONF_PT2} ] ; then
+	dqb "rm -rf ./${CONF_PT2} SOON"
+	csleep 3
+	rm -rf ./${CONF_PT2}
+	[ $? -gt 0 ] && exit
+fi
+
 csleep 2
 
 dqb "BFROE tig"
@@ -57,20 +65,18 @@ ${tig} clone ${branch} https://${CONF_BASEURL}/${CONF_PT2}.git
 dqb "TGI KO"
 csleep 2
 
-#020426:toimii
-mv minimize minimize.OLD
+mv ${CONF_BASE} ${CONF_BASE}.OLD
 mv ${CONF_PT2}/* .
 
-#DONE:voisi taas selvittää, ovatko 1take-haaran matskut toimintaqntoisia? enimmäkseen (130426)
-#TODO:sen man1.  - jutut kys- haaran e22:seen? vai sittenkin e/update2 else-haara hoitamaan?
-[ -s minimize/common_lib.sh ] && chmod 0555 minimize/common_lib.sh 
+[ -s ${CONF_LIB} ] && chmod 0555 ${CONF_LIB} 
 echo $?
 
 dqb "FN0C"
 csleep 1
 
-if [ -s minimize.OLD/${distro}/conf ] ; then
-	mv minimize.OLD/${distro}/conf minimize/${distro}/conf
+#oli kanssa se conf.example
+if [ -s ${CONF_BASE}.OLD/${distro}/conf ] ; then
+	mv ${CONF_BASE}.OLD/${distro}/conf ${CONF_BASE}/${distro}/conf
 else
 	dqb "N0.C0NF"
 fi
@@ -79,21 +85,18 @@ echo $?
 dqb "NEXT:common_lib"
 csleep 1
 
-if [ -x minimize/common_lib.sh ] ; then
+if [ -x ${CONF_LIB} ] ; then
 	#TODO:/o/b-juttuja oli kanssa
 
 	#josko nyt jo?
 	for d in $(find ${d0} -type f -name "*.sh") ; do chmod 0555 ${d} ; done
 
-	#140426:knftydston puute kenteis estää seur rivien toiminnan (tekisikö jotain?)
-	. minimize/common_lib.sh
-	enforce_access $(whoami) ${d0}/minimize
+	#TODO:konftdston kanssa jotain
+	. ${CONF_LIB}
+	enforce_access $(whoami) ${d0}/${CONF_BASE}
 
 	#josko nyt jo?
 	for d in $(find ${d0} -type f -name "*.sh") ; do chmod 0555 ${d} ; done
 else
-	echo "SMTHING WR0NG W/ minimize/common_lib"
+	echo "SMTHING WR0NG W/ ${CONF_LIB}"
 fi
-
-
-
