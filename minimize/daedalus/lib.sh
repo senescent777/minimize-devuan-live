da1d3cdc932bead6167d127dbd1b1323713e4b88
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-04-18 20:07:17 +0300

    after ... 2

--- minimize/daedalus/lib.sh
+++ minimize/daedalus/lib.sh
@@ -1,20 +1,28 @@
 #=================================================PART 0=====================================
-#
+
 #https://askubuntu.com/questions/952113/how-to-bypass-dpkg-prompt
 #https://askubuntu.com/questions/254129/how-to-display-all-apt-get-dpkgoptions-and-their-current-values
-#130126:sqrot-tstissä psmisc ei poistunut (miten nkyään? jos selvittäisi)
-#
+#130126:sqrot-tstissä psmisc ei poistunut (miten nkyään?)
+
 #020326:annetaanpa toistaiseksi olla PART175_LIST kuten on ja p_p2 myös
 function pre_part2() { #käytössä
 	dqb "daud.pre_part2()"
-	#csleep 2
-	#cg_pp2
+	csleep 2
 
+	${odio} /etc/init.d/ntpd stop
+	#$sharpy ntp* jo aiempana
+
+	for f in $(find /etc/init.d -type f -name 'ntp*' ) ; do 
+		${odio} ${f} stop
+		csleep 1
+	done
+
+	csleep 2
+	dqb "d0n3"
 }
-#
-#tekisikö tuolle josqs jotain vaiei?
+
 function tpc7() {
 	dqb "d.prc7 UNDER CONSTRUCTION"
 }
-#
-#lftr="${smr} -rf /run/live/medium/live/initrd.img* " 	
+
+lftr="echo # \${smr} -rf  / run / live / medium / live / initrd.img\* " 	
