578f0af70025827498321a549ac8a20b06f88bf1
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-05-03 13:59:19 +0300

    maybe this works?

--- minimize/e/export3.bash
+++ minimize/e/export3.bash
@@ -1,54 +1,67 @@
 #!/bin/bash
-
-debug=0 #1
+debug=0
 distro=$(cat /etc/devuan_version)
-
 d0=$(pwd)
 d=${d0}/${distro}
 mode=-2
 tgtfile=""
+gbk=0
 
 function usage() {
-	echo "$0 <mode> <tgtfile> [ -v] "
+	echo "$0 <mode> <tgtfile> [ -v] [-p] "
 	echo "mode:"
-	echo "f: makes archive of .deb-files"
+	echo "f: makes archive of .deb-Files"
 	echo "q: makes archive contaihing firefox profile"
 	echo "c: is sq-Chroot-env-related option"
-	echo "p:" Pulls \${CONF_default_archive3} from somewhere?"
-	
+	echo "p: Pulls \${CONF_default_archive3} from somewhere?"
 }
 
 function parse_opts_1() {
-	dqb "parse_opts_1( ${1})"
+	dqb "x3.popts1 ${1} "
+
+	case "${1}" in
+		-p)
+			#if [ ${gbk} -lt 1 ] ; then
+				gbk=1
+			#fi
+		;;
+		*)
+			#tämä voisi olla myös parse_opts_2:sessa?
+			if [ "${mode}" == "-2" ] ; then
+				mode=${1}
+			fi
+		;;
+	esac
 }
 
 function parse_opts_2() {
-	dqb "parse_opts_2)))))))( ${1} ; ${2} ))))))"
+	if [ -z "${tgtfile}" ] ; then
+		if [ "${2}" != "-v" ] ; then			
+			tgtfile=${2}
+		fi
+	fi
 }
 
-function fallback() { #tarpeellinen?
-	exit 59
-}
+if [ -x ${d0}/common_lib.sh ] ; then
+	. ${d0}/common_lib.sh
+else
+	exit 5
+fi
 
 [ -z "${distro}" ] && exit 6
 #d=${d0}/${distro} #nykyään vähän turha tässä
 process_lib ${d}
 
 if [ -x ${d0}/e/e22.sh ] ; then
-	.  ${d0}/e/e22.sh
-	[ $? -gt 0 ] && exit 66
-	csleep 1
-
+	.  ${d0}/e/e22.sh #tässä jotain vikaa vikaa? toiv ei
 	.  ${d0}/e/e23.sh
-	[ $? -gt 0 ] && exit 67
-	csleep 1
 else
 	echo "NO BACKEND FOUND"
 	exit 58
 fi
 
 e22_hdr ${tgtfile}
-[ -v CONF_iface ] && ${sifd} ${CONF_iface}
+#[ -v CONF_iface ] && ${sifd} ${CONF_iface} #toistaiseksi pois sotkemasta
 
 case "${mode}" in
 #	rp) #080326:toistaiseksi jemmaan, kiukuttelua (takaisin komm josqs?)
@@ -57,25 +70,24 @@ case "${mode}" in
 #		e22_rpg ${tgtfile} ${d}
 #17426:josqs ta,aisin kommenteista?
 #	;;
-	f) #170426:osaa tehdä paketin edelleen
+	f)
+		t=$(echo ${d} | cut -d "/" -f 1-5 | tr -d -c 0-9a-zA-Z/.)
 		enforce_access $(whoami) ${t}
 		e22_arch ${tgtfile} ${d} ${gbk}
-		
-		#HUOM! EIPÄ KIKKAILLA sha512sums.txt KANSSA, tar.sha PAREMPI IDEA
-		#, PITÄÄ VAIN SAADA AIKAISEKSI common_lib.ah HUOMIOIMAAN SE
 	;;
 	q)
-		#170326:tekee edelleen arkiston, sisältö kenties ok
 		[ -v CONF_default_arhcive ] || exit 33
 		[ -v CONF_default_arhcive2 ] || exit 34
 		[ -v CONF_default_arhcive3 ] || exit 35
-
 		e23_qrs ${tgtfile} ${d0} ${CONF_default_arhcive2} ${CONF_default_arhcive} ${CONF_default_arhcive3}
 	;;
-	c) #ainakin 160426 tIEnoilla toimi viimeksi
+	c)
 		e22_cde ${tgtfile} ${d0} ${distro}
+		mv ${tgtfile} ${tgtfile}.tmp
+		bzip2 -c -z ${tgtfile}.tmp > ${tgtfile}
+		[ $? -eq 0 ] && ${NKVD} ${tgtfile}.tmp
 	;;
-	p) #170326:lienee kunnossa
+	p)
 		[ -v CONF_default_arhcive3 ] || exit 66
 		csleep 1
 		[ -v CONF_iface ] && ${sifu} ${CONF_iface}
@@ -83,6 +95,9 @@ case "${mode}" in
 	;;
 	-h)
 		usage
+	;;
+	s)
+		e22_stu ${tgtfile} 
 	;;
 #	b)
 #		#230326:tekee jo jotain, vielä sietää miettiä onko siinä pointtia mitä tekee
