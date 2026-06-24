7be989af059ed6f20059a23abb4b8250115eec60
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-06-25 02:32:56 +0300

    recent cnagnes/e

--- minimize/e/e22.sh
+++ minimize/e/e22.sh
@@ -157,16 +157,26 @@ function e22_cleanpkgs() {
 }
 
 function e22_config1() {
-[ -z "${1}" ] && exit 11
-[ -d ${1} ] || exit 22
-[ -z "${2}" ] && exit 11
-local p
-p=$(pwd)
-cd ${1}
-[ -f ${1}/${2} ] && mv ${1}/${2} ${1}/${2}.ÅLD
-${sr0} -jcf ${2} ./xorg.conf* ./.config
-[ -s ${2} ] || exit 99
-cd ${p}
+	dqb "e22_config1()"
+	csleep 1
+
+	[ -z "${1}" ] && exit 11
+	[ -d ${1} ] || exit 22
+	[ -z "${2}" ] && exit 11
+
+	dqb "pars.ok"
+	csleep 1
+
+	local p=$(pwd)
+	cd ${1}
+
+	[ -f ${1}/${2} ] && mv ${1}/${2} ${1}/${2}.ÅLD
+	${sr0} -jcf ${2} ./xorg.conf* ./.config
+	[ -s ${2} ] || exit 99
+	cd ${p}
+
+	dqb "e22_config1() DONE"
+	csleep 1
 }
 
 #TODO:ffox 147? https://www.phoronix.com/news/Firefox-147-XDG-Base-Directory  
@@ -204,6 +214,9 @@ function e22_settings() {
 }
 
 function e22_home_pre() {
+	dqb "e22_home_pre()"
+	csleep 1
+
 	[ -z "${1}" ] && exit 67
 	[ -s ${1} ] || exit 68
 	[ -z "${2}" ] && exit 69
@@ -212,6 +225,9 @@ function e22_home_pre() {
 	[ -z "${4}" ] && exit 73
 	[ -z "${5}" ] && exit 79
 
+	dqb "pars.ok"
+	csleep 1
+
 	if [ ${3} -eq 1 ] && [ -d ${2} ] ; then
 		e22_config1 ~ ${4}
 		${NKVD} ~/${5}		
@@ -219,7 +235,6 @@ function e22_home_pre() {
 	fi
 
 	e_final
-
 	${srat} -rvf ${1} /opt/bin
 
 	for t in $(find ~ -type f -name merd2.sh | head -n 1) ; do
@@ -229,30 +244,43 @@ function e22_home_pre() {
 	for t in $(find ~ -type f -name ${4} ) ; do
 		${srat} -rvf ${1} ${t}
 	done
+
+	csleep 1
+	dqb "e22_home_pre()"
 }
 
 function e22_home() {
+	dqb "e22_home()"
+	csleep 1
+
 	[ -z "${1}" ] && exit 67
 	[ -s ${1} ] || exit 68
 	[ -z "${2}" ] && exit 69
 	[ -d ${2} ] || exit 70
 	[ -z "${3}" ] && exit 71
-	local t
+
+	dqb "pars.ok"
+	csleep 1
+
 	local f
 	${srat} -rvf ${1} ${2}/../${3}
-	t=$(${srat} -tf ${1} | grep ${3} | wc -l)
+	local t=$(${srat} -tf ${1} | grep ${3} | wc -l)
 	[ ${t} -lt 1 ] && exit 72
 	csleep 1
+
 	t=$(echo ${2} | tr -d -c 0-9a-zA-Z/ | cut -d / -f 1-5)
-	${srat} ${TARGET_TPX} --exclude "*.deb" --exclude "*.conf" -rvf ${1} /home/stubby ${t}
+	#TODO:TPX-kohdan kanssa jotain muutoksia vaiko ei?
+	${srat} "${TARGET_TPX}" --exclude "*.deb" --exclude "*.conf" -rvf ${1} /home/stubby ${t}
 	csleep 1
+
 	#miksi täsäs eokä h_pre() ?
 	for f in $(find ~ -type f -name "xorg.conf*" ) ; do ${srat} -rvf ${1} ${f} ; done
+
+	dqb "e22_home() DONE"
+	csleep 1
 }
 
 
-#toistaiseksi privaatti fktio (tarvitseeko kutsua suoraan exp2 kautta oikeastaan?)
-#120426:vissiin kopsaa kohteeseen mitä pitääkin
 
 function luca() {
 [ -z "${1}" ] && exit 11
@@ -263,19 +291,20 @@ for f in $(find /etc -type f -name "local*" -and -not -name "*.202*" ) ; do ${sr
 [ ${debug} -eq 1 ] && ${srat} -tf ${1} | grep local
 }
 
-#slim/xdm/wdm-spesifinen konfiguraatio saattaa tulla jo mukaan myös
-#020426:ei vedä verkosta mitään ni ei tartte lisätestejä?
-#140426:kopsannee kohteeseen mitä pitääkin
-#(meneekö rules.* kohteeseen useamman kerran?)
-#200426:taitaa toimia edelleen
-
 function e22_acol() {
+	dqb "e22_acol()"
+	csleep 1
+
 	[ -z "${1}" ] && exit 1
 	[ -s ${1} ] || exit 4 
 	#[ -w ${1} ] || exit 9
 	[ -z "${2}" ] && exit 2
 	[ -z "${3}" ] && exit 3		
 	[ -z "${4}" ] && exit 5
+
+	dqb "pars.ok"
+	csleep 1
+
 	${scm} 0555 /etc/iptables
 	${scm} 0444 /etc/iptables/rules*
 	${scm} 0444 /etc/default/rules*
@@ -283,7 +312,11 @@ function e22_acol() {
 	local f
 	local ef
 	local g
-	for f in $(find /etc -type f -name "interfaces*" -and -not -name "*.202*" ) ; do ${srat} -rvf ${1} ${f} ; done
+
+	for f in $(find /etc -type f -name "interfaces*" -and -not -name "*.202*" ) ; do 
+		${srat} -rvf ${1} ${f}
+	done
+
 	for f in $(${odio} find /etc -type f -name "rules*" -and -not -name "*.202*" ) ; do
 		if [ -s ${f} ] && [ -r ${f} ] ; then
 			${srat} -rvf ${1} ${f}
@@ -296,7 +329,9 @@ function e22_acol() {
 	if [ -r /etc/iptables ] || [ -w /etc/iptables ] || [ -r /etc/iptables/rules.v4 ] ; then
 		exit 112
 	fi
+
 	${srat} -rvf ${1} /etc/default/net*
+
 	case "${2}" in
 		wlan0)
 			${srat} -rvf ${1} /etc/wpa_supplicant
@@ -305,86 +340,106 @@ function e22_acol() {
 		*)
 		;;
 	esac
+
 	if [ ${3} -gt 0 ] ; then #-eq 1
 		for f in $(find /etc -type f -name "stubby*" -and -not -name "*.202*" ) ; do ${srat} -rf ${1} ${f} ; done
 		for f in $(find /etc -type f -name "dns*" -and -not -name "*.202*" ) ; do ${srat} -rf ${1} ${f} ; done
 	fi
+
 	ef=$(echo ${4} | tr -d -c 0-9)
+
 	if  [ ${ef} -eq 1 ] ; then
 		dqb "SMTHING"
 	else
 		${srat} -rf ${1} /etc/sudoers.d/meshuqqah /etc/fstab
 	fi
+
+	dqb "e22_acol() DONE"
+	csleep 1
 }
 
 [ -v CONF_BASEURL ] || exit 6
 
-#verkkoyhteyttä vaativat jutut vain jos testgris ei asetettu? vaiko kutsuvan koodin puolella tarkistus?
-#140426:muuten mennee pakettiin jutut paitsi dhclient-script?
-#160426:toimii
-
 function e22_ext() {
-[ -z "${1}" ] && exit 1
-[ -d ${1} ] && exit 59
-[ -f ${1} ] || exit 67
-[ -s ${1} ] || exit 2
-[ -w ${1} ] || exit 6
-[ -z "${2}" ] && exit 3
-[ -z "${3}" ] && exit 4
-[ -z "${4}" ] && exit 47
-[ -d ${4} ] && exit 53
-[ -f ${4} ] || exit 61
-local p
-local q	
-local r
-local st
-p=$(pwd)
-q=$(mktemp -d)
-r=$(echo ${2} | cut -d '/' -f 1 | tr -d -c a-zA-Z)
-st=$(echo ${3} | tr -d -c 0-9)
-[ ${debug} -eq 1 ] && pwd
-cd ${q}
-${tig} clone https://${CONF_BASEURL}/more_scripts.git
-[ $? -eq 0 ] || exit 66
-cd more_scripts/misc
-echo $?
-${spc} /etc/dhcp/dhclient.conf ./etc/dhcp/dhclient.conf.${st}
-if [ ! -s ./etc/dhcp/dhclient.conf.1 ] ; then
-${spc} ./etc/dhcp/dhclient.conf.new ./etc/dhcp/dhclient.conf.1	
-fi
-${spc} /etc/resolv.conf ./etc/resolv.conf.${st}
-if [ ! -s ./etc/resolv.conf.1 ] ; then
-${spc} ./etc/resolv.conf.new ./etc/resolv.conf.1
-fi
-${spc} /sbin/dhclient-script ./sbin/dhclient-script.${st}
-if [ ! -s ./sbin/dhclient-script.1 ] ; then
-${spc} ./sbin/dhclient-script.new ./sbin/dhclient-script.1
-ls -las ./sbin
-fi
-if [ -f /etc/apt/sources.list ] ; then
-local c
-c=$(grep -v '#' /etc/apt/sources.list | grep 'http:'  | wc -l)
-if [ ${c} -lt 1 ] ; then
-${spc} /etc/apt/sources.list ./etc/apt/sources.list.${2}
-fi
-fi
-${svm} ./etc/apt/sources.list ./etc/apt/sources.list.tmp
-${svm} ./etc/network/interfaces ./etc/network/interfaces.tmp
-${spc} /etc/network/interfaces ./etc/network/interfaces.${r}
-${sco} -R root:root ./etc
-${scm} -R a-w ./etc
-${sco} -R root:root ./sbin 
-${scm} -R a-w ./sbin
-${srat} -rvf ${1} ./etc  #./sbin jälkimmäinen hmisto josqs takaisin vai ei?
-echo $?
-local f
-for f in $(find ./etc -type f -not -name "interfaces.*" ) ; do
-${sah6} ${f} >> ${4}
-done
-cd ${p}
+	dqb "e22_ext()"
+	[ -z "${1}" ] && exit 1
+	[ -d ${1} ] && exit 59
+	[ -f ${1} ] || exit 67
+	[ -s ${1} ] || exit 2
+	[ -w ${1} ] || exit 6
+	[ -z "${2}" ] && exit 3
+	[ -z "${3}" ] && exit 4
+	[ -z "${4}" ] && exit 47
+	[ -d ${4} ] && exit 53
+	[ -f ${4} ] || exit 61
+	csleep 1
+	dqb "params ok"
+	csleep 1
+
+	local p
+	local q	
+	local r
+	local st
+
+	p=$(pwd)
+	q=$(mktemp -d)
+	r=$(echo ${2} | cut -d '/' -f 1 | tr -d -c a-zA-Z)
+	st=$(echo ${3} | tr -d -c 0-9)
+	[ ${debug} -eq 1 ] && pwd
+
+	cd ${q}
+	${tig} clone https://${CONF_BASEURL}/more_scripts.git
+	[ $? -eq 0 ] || exit 66
+
+	cd more_scripts/misc
+	echo $?
+
+	${spc} /etc/dhcp/dhclient.conf ./etc/dhcp/dhclient.conf.${st}
+
+	if [ ! -s ./etc/dhcp/dhclient.conf.1 ] ; then
+		${spc} ./etc/dhcp/dhclient.conf.new ./etc/dhcp/dhclient.conf.1	
+	fi
+
+	${spc} /etc/resolv.conf ./etc/resolv.conf.${st}
+
+	if [ ! -s ./etc/resolv.conf.1 ] ; then
+		${spc} ./etc/resolv.conf.new ./etc/resolv.conf.1
+	fi
+
+	${spc} /sbin/dhclient-script ./sbin/dhclient-script.${st}
+
+	if [ ! -s ./sbin/dhclient-script.1 ] ; then
+		${spc} ./sbin/dhclient-script.new ./sbin/dhclient-script.1
+		ls -las ./sbin
+	fi
+
+	if [ -f /etc/apt/sources.list ] ; then
+		local c
+		c=$(grep -v '#' /etc/apt/sources.list | grep 'http:'  | wc -l)
+
+		if [ ${c} -lt 1 ] ; then
+			${spc} /etc/apt/sources.list ./etc/apt/sources.list.${2}
+		fi
+	fi
+
+	${svm} ./etc/apt/sources.list ./etc/apt/sources.list.tmp
+	${svm} ./etc/network/interfaces ./etc/network/interfaces.tmp
+	${spc} /etc/network/interfaces ./etc/network/interfaces.${r}
+	${sco} -R root:root ./etc
+	${scm} -R a-w ./etc
+	${sco} -R root:root ./sbin 
+	${scm} -R a-w ./sbin
+	${srat} -rvf ${1} ./etc  #./sbin jälkimmäinen hmisto josqs takaisin vai ei?
+
+	echo $?
+	local f
+	for f in $(find ./etc -type f -not -name "interfaces.*" ) ; do
+		${sah6} ${f} >> ${4}
+	done
+
+	cd ${p}
 }
 
-#190426:lienee edelleen toimiva
 function e22_ts() {
 	[ -z "${1}" ] && exit 13
 	[ -d ${1} ] || exit 14
@@ -487,8 +542,7 @@ function e22_arch() {
 ##	exit
 #}
 
-#TODO:ao. fktion kanssa sitä self_extracting_archive-juttua kokeillen (JOKO JO 170426?)
-
+#TODO:TÄMÄ PASKA UUSIKSI JÄLLEEN KERRAN
 function e22_cde() {
 	dqb "e22_cde()"
 	
@@ -497,6 +551,7 @@ function e22_cde() {
 	[ -z "${3}" ] && exit 96
 	[ -d "${2}" ] || exit 97
 	[ -d "${3}" ] || exit 95
+
 	cd ${2}
 	fasdfasd ${1}
 	[ ${debug} -eq 1 ] && ls -las ${1}*
@@ -504,96 +559,141 @@ function e22_cde() {
 	${srat} --exclude "*merd*" -jcvf ${1} ./*.sh ./pkgs_drop ./${3}/*.sh
 }
 
-#1110426:jossain rikotaan /e/resolv.conf-linkki, voisi tehdä jotain qhan löytää missä (TODO?)
-#tässä tdstossa pre2() ja ext() , lisäksi common_lib kautta e_e -> enforce_access
-#-> merd2, g_doit, sq_rot, import2, export3, export2, e22_pre1()
-
 function z1() {
-[ -z "${1}" ] && exit 66
-csleep 2
-${NKVD} ${1}.tmp
-${spc} ${1} ${1}.ÅLD
-${spc} ${1}.sig ${1}.sig.ÅLD
-${spc} ${1}.sha ${1}.sha.ÅLD
-csleep 1
-fasdfasd ${1}.tmp
+	dqb "z1( ${1} )"
+	csleep 1
+
+	[ -z "${1}" ] && exit 66
+	dqb "pars ok"
+	csleep 2
+	${NKVD} ${1}.tmp
+
+	${spc} ${1} ${1}.ÅLD
+	${spc} ${1}.sig ${1}.sig.ÅLD
+	${spc} ${1}.sha ${1}.sha.ÅLD
+
+	csleep 1
+	fasdfasd ${1}.tmp
+
+	dqb "z1() DONE"
+	csleep 1
 }
 
 function z2() {
-[ -z "${1}" ] && exit 66
-reqwreqw ${1}.tmp
-csleep 1
-${NKVD} ${1}.sig
-${NKVD} ${1}.sha
-${NKVD} ${1}
-csleep 1
-fasdfasd ${1}.sig
-fasdfasd ${1}.sha
-${svm} ${1}.tmp ${1}
-csleep 1
-${sah6} --ignore-missing -c ${1}
-csleep 3
-e22_tyg ${1}
-${sah6} ${1} > ${1}.sha
+	dqb "z2 ( ${1}) "
+	csleep 1
+	[ -z "${1}" ] && exit 66
+	dqb "pars ok"
+	csleep 1
+
+	reqwreqw ${1}.tmp
+	csleep 1
+
+	${NKVD} ${1}.sig
+	${NKVD} ${1}.sha
+	${NKVD} ${1}
+	csleep 1
+
+	fasdfasd ${1}.sig
+	fasdfasd ${1}.sha
+	${svm} ${1}.tmp ${1}
+	csleep 1
+
+	${sah6} --ignore-missing -c ${1}
+	csleep 3
+
+	e22_tyg ${1}
+	${sah6} ${1} > ${1}.sha
+
+	dqb "z2 ( ${1}) DONE"
+	csleep 1
 }
 
 function z3() {
-[ -z "${1}" ] && exit 66
-[ -s ${2} ] || exit 67
-[ -z "${3}" ] && exit 68
-csleep 1
-fasdfasd ${3}
-csleep 1
-if [ ! -s ${3} ] ; then
-#tulöeeko export3 mukaan?
-${sr0} -tf ${2} | grep -v .tar | grep -v .deb > ${3}
-csleep 1
-fi
-${srat} -rvf ${2} ${3}
-local t=$(dirname ${1})
+	dqb "z3"
+	csleep 1
 
-#onko riittävästi renkattu kohteessa?
-${scm} go-w ${t}/*
-${sco} -R 0:0 ${1}
-${srat} -rvf ${2} ${1}*
-${scm} go-r ${t}/*
-csleep 1
+	[ -z "${1}" ] && exit 66
+	[ -s ${2} ] || exit 67
+	[ -z "${3}" ] && exit 68
+
+	dqb "oars ok"
+	csleep 1
+
+	fasdfasd ${3}
+	csleep 1
+
+	if [ ! -s ${3} ] ; then
+		#tulöeeko export3 mukaan?
+		${sr0} -tf ${2} | grep -v .tar | grep -v .deb > ${3}
+		csleep 1
+	fi
+
+	${srat} -rvf ${2} ${3}
+	local t=$(dirname ${1})
+
+	#onko riittävästi renkattu kohteessa?
+	${scm} go-w ${t}/*
+	${sco} -R 0:0 ${1}
+	${srat} -rvf ${2} ${1}*
+	${scm} go-r ${t}/*
+	csleep 1
+
+	dqb "z3 DONE"
+	csleep 1
 }
 
 #(josko exp2 voisi korvata "tar -T -cf":llä?)
 
-#200426:qseeko tässä jokin?
 function e22_sarram() {
-#[ -z "${1}" ] && exit 1
-[ -s ${1} ] || exit 4 
-#[ -w ${1} ] || exit 9
-[ -z "${2}" ] && exit 11
-[ -z "${3}" ] && exit 13
-[ -s ${3} ] || exit 17
-${srat} -rf ${1} /etc/init.d/net*
-${srat} -rf ${1} /etc/rcS.d/S*net*
-csleep 1
-local f
-for f in $(${odio} find /etc -type f -name "xorg*" -and -not -name "*.202*" ) ; do
-${srat} -rvf ${1} ${f}
-done
-csleep 1
-for f in $(${odio} find /etc -type f -name "${2}*" -and -not -name "*.202*" ) ; do
-${srat} -rvf ${1} ${f}
-done
-${srat} -rvf ${1} /etc/X11/default-display-manager
-#HUOM.tätä ao. 3 riviä varten oli valmiskin palikka?
-${scm} 0555 /etc/iptables
-${scm} 0400 /etc/iptables/rules*
-${scm} 0400 /etc/default/rules*
-for f in $(${odio} find /etc -type f -name "rules.v?.?" -and -not -name "*.202*" ) ; do ${sah6} ${f} >> ${3} ; done
-for f in $(find ~ -type f -name "*pkgs*" | grep -v .OLD ) ; do ${sah6} ${f} >> ${3} ; done
-#230326:ntp-jtut näyttäisi vetävän mukaan
-if [ -x /usr/sbin/ntpd ] ; then
-for f in $(${odio} find /etc -type f -name "ntp*" ) ; do
-${srat} -rvf ${1} ${f}
-${sah6} ${f} >> ${3}
-done
-fi
-other_horrors
+	dqb "e22_sarram()"
+	csleep 1
+
+	#[ -z "${1}" ] && exit 1
+	[ -s ${1} ] || exit 4 
+	#[ -w ${1} ] || exit 9
+	[ -z "${2}" ] && exit 11
+	[ -z "${3}" ] && exit 13
+	[ -s ${3} ] || exit 17
+
+	dqb "PARAMS ok"
+	csleep 1
+
+	${srat} -rf ${1} /etc/init.d/net*
+	${srat} -rf ${1} /etc/rcS.d/S*net*
+
+	csleep 1
+	local f
+
+	for f in $(${odio} find /etc -type f -name "xorg*" -and -not -name "*.202*" ) ; do
+		${srat} -rvf ${1} ${f}
+	done
+
+	csleep 1
+
+	for f in $(${odio} find /etc -type f -name "${2}*" -and -not -name "*.202*" ) ; do
+		${srat} -rvf ${1} ${f}
+	done
+
+	${srat} -rvf ${1} /etc/X11/default-display-manager
+
+	#HUOM.tätä ao. 3 riviä varten oli valmiskin palikka?
+	${scm} 0555 /etc/iptables
+	${scm} 0400 /etc/iptables/rules*
+	${scm} 0400 /etc/default/rules*
+
+	for f in $(${odio} find /etc -type f -name "rules.v?.?" -and -not -name "*.202*" ) ; do ${sah6} ${f} >> ${3} ; done
+	for f in $(find ~ -type f -name "*pkgs*" | grep -v .OLD ) ; do ${sah6} ${f} >> ${3} ; done
+
+	if [ -x /usr/sbin/ntpd ] ; then
+		for f in $(${odio} find /etc -type f -name "ntp*" ) ; do
+			${srat} -rvf ${1} ${f}
+			${sah6} ${f} >> ${3}
+		done
+	fi
+
+	other_horrors
+
+	dqb "e22_sarram() DONE"
+	csleep 1
 }
--- minimize/e/export2.sh
+++ minimize/e/export2.sh
@@ -168,7 +168,7 @@ case "${mode}" in
 		reqwreqw /opt/bin/zxcv.tmp
 
 		#HUOM.31725:jatkossa jos vetelisi paketteja vain jos $d alta ei löydy?
-		if [ ${mode} -eq 3 ] && [ ! -v CONF_testgris ] ; then
+		if [ ${mode} -eq 3 ] && [ ! -v CONF_testgris ] ; then #jälk ehtoon muutoksia vai ei?
 			e23_tblz ${d} ${CONF_iface} ${distro} ${CONF_dnsm}
 			e23_other_pkgs ${CONF_dnsm}
 		else
