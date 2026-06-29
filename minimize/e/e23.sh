b00f6b7fe78ea2a58031cceabc7f6fd4302666c9
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-06-29 16:03:24 +0300

    Add files via upload

--- minimize/e/e23.sh
+++ minimize/e/e23.sh
@@ -1,5 +1,3 @@
-#just_download_not_install-vipu olisi tietysti...
-
 function aswasw() {
 	dqb "aswasw( ${1} )"
 	[ -z "${1}" ] && exit 56
@@ -9,7 +7,7 @@ function aswasw() {
 		wlan0)
 			#E22:GN="libnl-3-200 ... "
 			#https://pkginfo.devuan.org/cgi-bin/package-query.html?c=package&q=wpasupplicant=2:2.10-12+deb12u2
-			
+
 			${shary} libnl-3-200 libnl-genl-3-200 libnl-route-3-200 libpcsclite1 #libreadline8 # libssl3 adduser
 			${shary} wpasupplicant
 		;;
@@ -18,36 +16,35 @@ function aswasw() {
 	esac
 }
 
-#TODO:vissiin muistettava toisessa oksassa katsoa kuinka monta param tälle fktiolle siellä menee
 function e23_tblz() {
-	dqb "; )e23_tblz( ( ${1} ( ${2} (((  ${3} )( (((  ${4}   )"
+	dqb "e23_tblz()"
 	csleep 1
 
 	[ -z "${1}" ] && exit 11
+	[ -d ${1} ] || exit 15
 	[ -z "${2}" ] && exit 12
+	[ -z "${3}" ] && exit 13
+	[ -z "${4}" ] && exit 14 #HUOM.tämän trapeellisuus?
 
 	${fib}
 	${asy}
-	csleep 1
+	#csleep 1
 
 	#message() tähän?
-	tpc7
-	#jotain excaliburiin liittyvää tuo tpc
+	tpc7	#jotain excaliburiin liittyvää
+	aswasw ${2}
+	#TODO:isc-dhcp-pakettien mukaanotto riippumaan CONF_iface:sta?
+	${shary} ${E22_GT}
 
-	#$1 vai $2?
-	aswasw ${1}
-	#VAIH:isc-dhcp-pakettien mukaanotto riippumaan CONF_iface:sta?
-	e22_pre_e ${E22_GT}
-
+	[ ${debug} -eq 1 ] && ls -las ${CONF_pkgdir}
 	csleep 1
 
 	${asy}
-	# (2 param kutsussa sopisi riittää)
-	e22_pre2 ${1} ${2}
+	#actually necessary
+	e22_pre2 ${1} ${3} ${2} ${4}
 	other_horrors
 
-	csleep 1
-	dqb "e23_tblz() DONE"
+	dqb "e23_tblz()"
 }
 
 function e23_other_pkgs() { 
@@ -60,10 +57,13 @@ function e23_other_pkgs() {
 	csleep 1
 
 	#josko jollain optiolla saisi apt:in lataamaan paketit vain leikisti? --simulate? tai --no-download?
-	e22_pre_e ${E22_GI}
+	${shary} ${E22_GI}
 	E22_GG="coreutils libcurl3-gnutls libexpat1 liberror-perl libpcre2-8-0 git-man git"
 	e22_pre_e ${E22_GG}
-	e22_pre_e ${E23_GS}
+
+	#rämän eiirto-> common_lib? TAAS?
+	E23_GS="zlib1g libreadline8 groff-base libgdbm6 libpipeline1 libseccomp2 libaudit1 libselinux1 man-db sudo"
+	${shary} ${E23_GS}  #moni pak tarttee nämä
 
 	message
 	jules
@@ -90,6 +90,7 @@ function e23_other_pkgs() {
 
 function e23_upgp() {
 	dqb " e23_upgp() "
+
 	${fib}
 	csleep 1
 
@@ -113,7 +114,9 @@ function e23_upgp2() {
 		;;
 		*)
 			${NKVD} ${1}/wpa*
-	;;
+			#HUOM.25725:pitäisi kai poistaa wpa-paketit tässä, aptilla myös?
+			#... vai lähtisikö vain siitä että g_pt2 ajettu ja täts it
+		;;
 	esac
 
 	dqb " e23_upgp2() done"
@@ -129,11 +132,11 @@ function e23_qrs() {
 	[ -z "${2}" ] && exit 11
 	[ -d ${2} ] || exit 22
 	[ -z "${3}" ] && exit 44
-
+	#[ -f ${3} ] || exit 33
 	[ -z "${4}" ] && exit 43
-
+	#[ -f ${4} ] || exit 34
 	[ -z "${5}" ] && exit 43
-
+	#[ -f ${5} ] || exit 34
 
 	dqb "pars.0k"
 	csleep 1
@@ -142,7 +145,7 @@ function e23_qrs() {
 	${srat} -rvf ${1} ~/${3}
 	csleep 1
 
-
+	#tuleeko mukaan vai ei?
 	tar -tf ${1} | grep ${3} | wc -l
 	csleep 2
 
@@ -151,7 +154,9 @@ function e23_qrs() {
 	csleep 1
 
 	e22_settings ${2} ${4} ${5}
-	
+	#btw. mikä olikaan syy että q on tässä ekassa switch-case:ssa? pl siis että turha apt-renkkaus
+
+	#jospa ei hipsuja tähän find:iin
 	for f in $(find ${2} -maxdepth 1 -type f -name ${4} -or -name ${5} | grep -v pulse) ; do
 		${srat} -rvf ${1} ${f}
 	done
@@ -160,14 +165,11 @@ function e23_qrs() {
 	csleep 1
 }
 
-#pitää sitten jaksaa muistaa että tämän fktion tuotoksen asentuminen riippuu niistä accept-tdstoista kanssa
-
 function e23_dm() {
 	dqb "e23_dm())) ${1} )"
 	[ -z "${1}" ] && exit 11
 	csleep 2
 
-	#pitäisikö mennä näin kuitenkin?
 	${fib}
 	e22_pre_e ${E22_GS}
 	e22_pre_e ${E22_GM}
@@ -180,7 +182,10 @@ function e23_dm() {
 		exit 666
 	fi
 
+	#jos ei ala muuten sujua ni ao riveistä mallia accept1:seen
+	
 	${shary} libpango-1.0-0 libpangoft2-1.0-0 libpangoxft-1.0-0
+	#[ $? -eq 0 ] || exit 54 #to state the obvious:initramfs-kikkailujen takia ei kande nöin tehdö
 	${shary} libmagickcore-6.q16-6 libmagickwand-6.q16-6
 
 	${shary} libnuma1 libx265-199 libwraster6 libwings3
@@ -194,6 +199,7 @@ function e23_dm() {
 	csleep 10
 
 	${shary} libx11-6 libx11-xcb1 libx11-data libxext6 imagemagick-6-common libxmu6 libxmuu1 libgif7 libxpm4
+	#[ $? -eq 0 ] || exit 57 #jospa ei tämmöisiä tähän fktioon, tökkii
 	csleep 5
 
 	${shary} fontconfig fontconfig-config
@@ -220,13 +226,12 @@ function e23_dm() {
 
 	${shary} libxcursor1 libwutil5 man-db wmaker-common #libbz2-1.0
 	csleep 10
-	
+
 	${shary} libicu72 libxfixes3 libxml2
+	${shary} 
 	csleep 5
 
-	${shary} libglx-mesa0 libffi8 libzvbi0 git-man
-	${shary} libdb5.3 debconf libdeflate0 liblerc4 #mukaan?	
-	${shary} libpam-runtime #E22_GM toisi pari libpam-pakettttia
+	${shary} libpam-runtime #E22_GM toisi pari libpam-pakettttiaq
 	csleep 10
 
 	${shary} libxdmcp6 menu twm libmd0
@@ -261,24 +266,13 @@ function e23_dm() {
 	csleep 1
 }
 
-#TODO:miten se Makefile-idea? kokeilisiko?
-
 function e23_profs() {
-	dqb ";e23_profs) ${1} , ${2} , ${3} (()("
+	dqb "e23_profs) $1 , $2 , $3 ("
 	csleep 1
-
-	[ -z "${1}" ] && exit 76
-	[ -z "${2}" ] && exit 75
-	[ -z "${3}" ] && exit 74
-
-	[ -d "${2}" ] || exit 73
-	[ -s ${1} ] || exit 72
-	[ -s ${3} ] || exit 71
-
 	dqb "pars.0k"
 	csleep 1
 
-	q=$(${mkt} -d)
+	q=$(mktemp -d)
 	cd ${q}
 
 	[ $? -eq 0 ] || exit 77
@@ -300,6 +294,7 @@ function e23_profs() {
 	dqb "e23_profs() done"
 	csleep 1
 }
+
 
 function e23_st() { #120626:vissiin asentivat nämä paketit 
 	${shary} liblz4-1 liblzma5 liblzo2-2 libzstd1 squashfs-tools
