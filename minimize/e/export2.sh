baf03b0866d1ce9d47fc0bd02ae108f9e16f0986
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-06-29 15:48:41 +0300

    Add files via upload

--- minimize/e/export2.sh
+++ minimize/e/export2.sh
@@ -23,7 +23,7 @@ function usage() {
 	echo "$0 -h: shows tHis message about usage"
 }
 
-#jos muuttaisi blokin koskapa gpo() nykyään? (-h kanssa voisi tehdä toisinkin)
+#VAIH:jos muuttaisi blokin koskapa gpo() nykyään? (-h kanssa voisi tehdä toisinkin)
 #... jospa ensin export3:sen kanssa kokeilut ja sitttten
 
 if [ $# -gt 1 ] ; then
@@ -60,6 +60,7 @@ function parse_opts_2() {
 			mop=${2}
 		;;
 #		*)
+#			#
 #			if [ "${mode}" == "-2" ] ; then
 #				mode=${1}
 #				tgtfile=${2}
@@ -70,6 +71,7 @@ function parse_opts_2() {
 	esac
 }
 
+#parsetuksen knssa menee jännäksi jos conf pitää ladata ennen common_lib (no parse_opts:iin tiettty muutoksia?)
 d=${d0}/${distro}
 
 function fallback() { #tarpeellinen?
@@ -83,28 +85,26 @@ else
 fi
 
 [ -z "${distro}" ] && exit 6
-d=${d0}/${distro} #nykyään vähän turha tässä
+d=${d0}/${distro}
 process_lib ${d}
-mop=${CONF_dm} 
+mop=${CONF_dm}
 
 dqb "BEF0RE T1G N0R MKTMP"
 sleep 1
 
 if [ -z "${tig}" ] ; then
-	echo "SHOULD INSTALL GIT ($0 e)"
+	echo "SHOULD INSTALL GIT"
 	[ "${mode}" == "e" ] || exit 7
 fi
 
 if [ -z "${mkt}" ] ; then
-	echo "SHOULD INSTALL MKTEMP ($0 e)"
+	echo "SHOULD INSTALL MKTEMP"
 	exit 8
 fi
 
 echo "JUST BEFORE INCLUDING FLIES 1nt0 50UP"
 sleep 1
 
-E22_GG="coreutils libcurl3-gnutls libexpat1 liberror-perl libpcre2-8-0 git-man git"
-	
 if [ -x ${d0}/e/e22.sh ] ; then
 	.  ${d0}/e/e22.sh
 	[ $? -gt 0 ] && exit 66
@@ -118,15 +118,21 @@ else
 	exit 58
 fi
 
+#https://askubuntu.com/questions/1206167/download-packages-without-installing liittynee
 [ -z "${tgtfile}" ] && exit 98
 t=$(echo ${d} | cut -d '/' -f 1-5)
 
+cont=0
+dqb "ESAC1"
 csleep 1
 [ -d ${d0}/${tgtfile} ] && exit 64
 
+#-h pysähtyy ennen tätä riviä?
 e22_hdr ${tgtfile}
 [ -v CONF_iface ] && ${sifd} ${CONF_iface}
+#jokin varmistus vielä että iface alhaalla?
 
+#HUOM!!! e22_pre2() AJAA sifu-KOMENNON JOTEN TÄSSÄ EI ERIKSEEN TARVITSE
 e22_pre1 ${d} ${distro}
 [ ${debug} -eq 1 ] && pwd;sleep 6
 
@@ -137,20 +143,17 @@ e22_cleanpkgs ${CONF_pkgdir}
 #HUOM.nämä voivat jtnkin suhtautua ylempään e22_hdr()-qtsuun jossia n tilanteessa
 [ -f ${d}/e.tar ] && ${NKVD} ${d}/e.tar
 [ -f ${d}/f.tar ] && ${NKVD} ${d}/f.tar
-
 doit=1
 csleep 1
-#getopt .o "34uetglnxs" ...
+
+dqb "JUST BEFORE ESAC"
+csleep 6
 
 case "${mode}" in
 	0)
 		exit 97
 	;;
-	3|4) 
-		
-		#TODO:main-oksan kanssa testaus josqs (merd2+exp2)
-		#VAIH:turhia kommentteja wttuun sotkemasta
-
+	3|4) 	
 		[ -v CONF_default_arhcive3 ] || exit 66
 		z1 /opt/bin/zxcv
 
@@ -158,15 +161,12 @@ case "${mode}" in
 		reqwreqw /opt/bin/zxcv.tmp
 
 		#HUOM.31725:jatkossa jos vetelisi paketteja vain jos $d alta ei löydy?
-		
-		if [ ${mode} -eq 3 ] && [ "${CONF_env}" == "DEFAULT" ] ; then
-			#TODO:tähän alle ehkä joskus muutoksia, rekursion tarkiotus liittyä
-
-			e23_tblz
+		if [ ${mode} -eq 3 ] && [ ! -v CONF_testgris ] ; then #jälk ehtoon muutoksia vai ei?
+			e23_tblz ${CONF_iface} ${CONF_dnsm}
 			e23_other_pkgs ${CONF_dnsm}
 		else
 			doit=0
-		fi	
+		fi
 		
 		e22_home_pre ${tgtfile} ${d} ${CONF_enforce} ${CONF_default_arhcive2} ${CONF_default_arhcive}
 		e22_home ${tgtfile} ${d} ${CONF_default_arhcive} 
@@ -192,30 +192,29 @@ case "${mode}" in
 	e) 
 		e22_pre_e ${E22_GS}
 		e22_pre_e ${E22_GM}
+
 		csleep 3
 		message
 		csleep 2
 
-		e23_tblz
-		dqb "BC/AD"
-		csleep 10
+		e23_tblz ${CONF_iface} ${CONF_dnsm}
 		e23_other_pkgs ${CONF_dnsm}
 	;;
 	t)
 		message
 		csleep 2
-		e23_tblz
+		e23_tblz ${CONF_iface} ${CONF_dnsm}
 	;;
 	g)
 		[ -v E22_GI ] || exit 95
+		#VAIH (muodostetun paketin toimivuuden testaus lähinnä)
 		e22_hdr ${d}/e.tar
 
 		${fib}
 		${shary} ${E22_GI} #ei tarvinne tässä pre_e kautta mennä
-		${shary} ${E22_GG}
-
 		e22_dblock ${d}/e.tar ${d} ${CONF_pkgdir} ${gbk}
 		${srat} -rvf ${tgtfile} ${d}/e.tar*
+
 		doit=0
 	;;
 	l)
@@ -224,35 +223,34 @@ case "${mode}" in
 		e23_dm ${mop}
 	;;
 	n)
-		#VAIH:ntp-jutut takaisin josqs? 260526 -> ?
+		#VAIH:ntp-jutut takaisin josqs?
 		${shary} lsb-base netbase python3 python3-ntp tzdata libbsd0 libcap2 libssl3
 		${shary} ntpsec
 	;;
 #	x)
-#		#:uusiksi vain koko pasq?
+#		#TODO:uusiksi vain koko pasq?
 #		e23_xyz
 #	;;
-	s) #080626:testit vähitellen käytniin tämän kanssa
+	s) #lienee tekevän toimivaa oksennusta (28626)
 		e23_st
 	;;
 	*)
-		echo "MAYBE U SHOULD USE export3 INSTEAD"
-		sleep 5
-		${d0}/export3.bash ${mode} ${tgtfile} -v
 		exit
 	;;
 esac
 
-#tuossa alla vielä jotain laittoa?
 if [ -d ${d} ] && [ ${doit} -eq 1 ] ; then 
 	e22_hdr ${d}/f.tar
 
 	e22_dblock ${d}/f.tar ${d} ${CONF_pkgdir} ${gbk}
 	e22_ftr ${d}/f.tar
+	#z3?	
 
 	${srat} -rvf ${tgtfile} ${d}/f.tar* 
 	[ $? -eq 0 ] && ${NKVD} ${d}/f.tar* 
 fi
+
+#exit
 
 if [ -s ${tgtfile} ] ; then
 	e22_ftr ${tgtfile}
